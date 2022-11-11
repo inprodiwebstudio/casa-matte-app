9974ad5e8afc2b2db8bcadb948d55ad4956336da
JoabMedel <joab_medel99@hotmail.com>
2022-11-11 11:38:10 -0600

    create hovered item page container

--- src/components/PaginatorBar/PaginatorBar.jsx
+++ src/components/PaginatorBar/PaginatorBar.jsx
@@ -6,7 +6,11 @@ const PaginatorBar = () => {
 		<div id="PaginatorBar">
 			<h3 className="header-ittle-paginator">PROYECTO</h3>
 			<div className="navbar-paginator-container">
-				Conatiner
+				<div className="item-page-container">&nbsp;</div>
+				<div className="item-page-container">&nbsp;</div>
+				<div className="item-page-container">&nbsp;</div>
+				<div className="item-page-container">&nbsp;</div>
+				<div className="item-page-container">&nbsp;</div>
 			</div>
 		</div>
 	);
