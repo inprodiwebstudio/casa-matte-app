6285201ae8bf038b594925543ed7a04e0b374136
JoabMedel <joab_medel99@hotmail.com>
2022-11-11 12:50:25 -0600

    add cover in white page drager and delete icon

--- src/Resources/icons/index.js
+++ src/Resources/icons/index.js
@@ -2,9 +2,13 @@ import SaveIcom from "./SaveIcon";
 import CarIcon  from "./CarIcon";
 import ArrowTop from "./ArrowTop";
 import PlusIcon from "./PlusIcon";
+import Cross    from "./Cross";
+import DragIcon from "./DragIcon";
 
 export {
+	Cross,
 	CarIcon,
+	DragIcon,
 	SaveIcom,
 	ArrowTop,
 	PlusIcon,
--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -1,14 +1,23 @@
 //Own Components
+import { DragIcon, Cross } from "Resources/icons";
 import "./ItemPage.scss";
 
 const ItemPage = () => {
 	return (
 		<div id="ItemPage">
-			<div>
-				<div className="withe-page-container">&nbsp;</div>
-				<div className="pages-book-conatier">
-					<p>1</p>
-					<p>2</p>
+			<div className="my-page-container">
+				<div className="drag-icon-conatainer">
+					<DragIcon size="15px" />
+				</div>
+				<div>
+					<div className="withe-page-container">&nbsp;</div>
+					<div className="pages-book-conatier">
+						<p>1</p>
+						<p>2</p>
+					</div>
+				</div>
+				<div className="cross-icon-conatiner">
+					<Cross size="9px" />
 				</div>
 			</div>
 		</div>
