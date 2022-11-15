027727bc7196dbe1a311326f2e6b6c5844637846
JoabMedel <joab_medel99@hotmail.com>
2022-11-15 15:25:16 -0600

    resolved drag and drop and scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { PersistGate }   from "redux-persist/integration/react";
 import store, { persistor } from "store";
 import Router               from "routes";
 import "./Resources/scss/index.scss";
+import "react-perfect-scrollbar/dist/css/styles.css";
+
 
 export default function App() {
 	return (
--- src/components/PaginatorBar/PaginatorBar.jsx
+++ src/components/PaginatorBar/PaginatorBar.jsx
@@ -1,64 +1,62 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import PerfectScrollbar                          from "react-perfect-scrollbar";
 
 //Own components
 import ItemPage from "components/ItemPage";
 import "./PaginatorBar.scss";
 
 const PaginatorBar = () => {
-	const onDragEnd = () => {
-		console.log("endDrag");
-	};
 	return (
-		<DragDropContext
-			onDragEnd={()=> onDragEnd()}
-		>
+		<DragDropContext>
 			<div id="PaginatorBar">
 				<h3 className="header-ittle-paginator">PAGINADO</h3>
 				<Droppable droppableId="box-droppable-1">
-					{(provided, snapshot) => (
-						<div
-							className="navbar-paginator-container"
-							ref={provided.innerRef}
-							{...provided.droppableProps}
-						>
-							<Draggable draggableId="darg-1" index={0}>
-								{(otherProvided) => (
-									<ItemPage
-										draggableProps={otherProvided.draggableProps}
-										dragHandleProps={otherProvided.dragHandleProps}
-										innerRef={otherProvided.innerRef}
-									/>
-								)}
-							</Draggable>
-							<Draggable draggableId="darg-2" index={1}>
-								{(otherProvided) => (
-									<ItemPage
-										draggableProps={otherProvided.draggableProps}
-										dragHandleProps={otherProvided.dragHandleProps}
-										innerRef={otherProvided.innerRef}
-									/>
-								)}
-							</Draggable>
-							<Draggable draggableId="darg-3" index={2}>
-								{(otherProvided) => (
-									<ItemPage
-										draggableProps={otherProvided.draggableProps}
-										dragHandleProps={otherProvided.dragHandleProps}
-										innerRef={otherProvided.innerRef}
-									/>
-								)}
-							</Draggable>
-							<Draggable draggableId="darg-4" index={3}>
-								{(otherProvided) => (
-									<ItemPage
-										draggableProps={otherProvided.draggableProps}
-										dragHandleProps={otherProvided.dragHandleProps}
-										innerRef={otherProvided.innerRef}
-									/>
-								)}
-							</Draggable>
-							{provided.placeholder}
-						</div>
+					{(provided) => (
+						<PerfectScrollbar>
+							<div
+								className="navbar-paginator-container"
+								ref={provided.innerRef}
+								{...provided.droppableProps}
+							>
+								<Draggable draggableId="darg-1" index={0}>
+									{(otherProvided) => (
+										<ItemPage
+											draggableProps={otherProvided.draggableProps}
+											dragHandleProps={otherProvided.dragHandleProps}
+											innerRef={otherProvided.innerRef}
+										/>
+									)}
+								</Draggable>
+								<Draggable draggableId="darg-2" index={1}>
+									{(otherProvided) => (
+										<ItemPage
+											draggableProps={otherProvided.draggableProps}
+											dragHandleProps={otherProvided.dragHandleProps}
+											innerRef={otherProvided.innerRef}
+										/>
+									)}
+								</Draggable>
+								<Draggable draggableId="darg-3" index={2}>
+									{(otherProvided) => (
+										<ItemPage
+											draggableProps={otherProvided.draggableProps}
+											dragHandleProps={otherProvided.dragHandleProps}
+											innerRef={otherProvided.innerRef}
+										/>
+									)}
+								</Draggable>
+								<Draggable draggableId="darg-4" index={3}>
+									{(otherProvided) => (
+										<ItemPage
+											draggableProps={otherProvided.draggableProps}
+											dragHandleProps={otherProvided.dragHandleProps}
+											innerRef={otherProvided.innerRef}
+										/>
+									)}
+								</Draggable>
+								{provided.placeholder}
+							</div>
+						</PerfectScrollbar>
 					)}
 				</Droppable>
 			</div>
