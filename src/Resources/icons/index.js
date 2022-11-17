b97bdac1e2c5b03d087b102afed3cc345c017f4a
JoabMedel <joab_medel99@hotmail.com>
2022-11-17 16:10:29 -0600

    change droped display component when is available images

--- src/Resources/icons/index.js
+++ src/Resources/icons/index.js
@@ -1,18 +1,21 @@
-import SaveIcom from "./SaveIcon";
-import CarIcon  from "./CarIcon";
-import ArrowTop from "./ArrowTop";
-import PlusIcon from "./PlusIcon";
-import Cross    from "./Cross";
-import DragIcon from "./DragIcon";
-import DropFile from "./DropFile";
-
+import SaveIcom  from "./SaveIcon";
+import CarIcon   from "./CarIcon";
+import ArrowTop  from "./ArrowTop";
+import PlusIcon  from "./PlusIcon";
+import Cross     from "./Cross";
+import DragIcon  from "./DragIcon";
+import DropFile  from "./DropFile";
+import PhotoList from "./PhotoList";
+import Folder    from "./Folder";
 
 export {
 	Cross,
+	Folder,
 	CarIcon,
 	DropFile,
 	DragIcon,
 	SaveIcom,
 	ArrowTop,
 	PlusIcon,
+	PhotoList,
 };
--- src/components/Gallery/DropDoc/DropDoc.jsx
+++ src/components/Gallery/DropDoc/DropDoc.jsx
@@ -2,8 +2,8 @@ import { useState }    from "react";
 import { useDropzone } from "react-dropzone";
 
 //Own components
-import { DropFile }     from "Resources/icons";
-import { isValidArray } from "helpers";
+import { DropFile, Folder, PhotoList } from "Resources/icons";
+import { isValidArray }                from "helpers";
 import "./DropDoc.scss";
 
 const DropDoc = () => {
@@ -37,17 +37,38 @@ const DropDoc = () => {
 		},
 	});
 
-	console.log(fileImage);
 	return (
 		<div className="DropDoc">
-			<div  {...getRootProps({className : "indicator-drop-container"})}>
-				<DropFile size="40px" />
-				<p>
-					ARRASTRA AQUÍ LAS FOTOGRAFÍAS QUE
-					QUIERAS AGREGAR A TU PROYECTO
-				</p>
-				<input {...getInputProps()} />
-			</div>
+			{
+				!isValidArray(fileImage) ? (
+					<div  {...getRootProps({className : "indicator-drop-container"})}>
+						<DropFile size="40px" />
+						<p>
+							ARRASTRA AQUÍ LAS FOTOGRAFÍAS QUE
+							QUIERAS AGREGAR A TU PROYECTO
+						</p>
+						<input {...getInputProps()} />
+					</div>
+				) : (
+					<div className="options-cards-container">
+						<div className="options-card">
+							<div className="cardOption">
+								<PhotoList size="50px" />
+								<p>
+									CARGAR A GALERÍA
+								</p>
+							</div>
+							<div className="cardOption">
+								<Folder size="50px" />
+								<p>
+									CARGAR EN UNA NUEVA
+									CARPETA
+								</p>
+							</div>
+						</div>
+					</div>
+				)
+			}
 		</div>
 	);
 };
