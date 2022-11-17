21c32173dde8064e06a1821c011f22ea3a40ee8e
JoabMedel <joab_medel99@hotmail.com>
2022-11-17 14:15:15 -0600

    resolved drop images

--- src/components/Gallery/DropDoc/DropDoc.jsx
+++ src/components/Gallery/DropDoc/DropDoc.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import { useState }    from "react";
+import { useDropzone } from "react-dropzone";
 
 //Own components
-import { DropFile } from "Resources/icons";
+import { DropFile }     from "Resources/icons";
+import { isValidArray } from "helpers";
 import "./DropDoc.scss";
 
 const DropDoc = () => {
+
+	const [ fileImage, setFileImage ] = useState([]);
+
+	const handleDrop = files => {
+		const isValidFiles = isValidArray(files);
+
+		if (isValidFiles) {
+			const newListFiles = files.map(file => {
+				const myFile = file;
+				const createObjectURL = Object.assign(myFile, { preview : URL.createObjectURL(myFile) });
+				return (
+					createObjectURL
+				);
+			});
+
+			setFileImage(prev => {
+				const newData = [...prev, ...newListFiles];
+				return newData;
+			});
+		}
+	};
+
+	const { getInputProps, getRootProps } = useDropzone({
+		multiple : true,
+		onDrop   : (files) => handleDrop(files),
+		accept   : {
+			"image/*" : [],
+		},
+	});
+
+	console.log(fileImage);
 	return (
 		<div className="DropDoc">
-			<div className="indicator-drop-container">
+			<div  {...getRootProps({className : "indicator-drop-container"})}>
 				<DropFile size="40px" />
 				<p>
 					ARRASTRA AQUÍ LAS FOTOGRAFÍAS QUE
 					QUIERAS AGREGAR A TU PROYECTO
 				</p>
+				<input {...getInputProps()} />
 			</div>
 		</div>
 	);
