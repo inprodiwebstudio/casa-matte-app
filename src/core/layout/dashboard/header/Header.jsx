2d6bb30b6d73e4e2c1777f62605c187190fcce70
JoabMedel <joab_medel99@hotmail.com>
2022-11-22 12:04:18 -0600

    change logo

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 	return (
 		<div className="Header">
 			<div className="body-container">
-				<h1 className="casa-matte-tittle">CASA MATTE</h1>
+				<img src="https://casamatte.com/wp-content/uploads/2021/02/logo_casamatte-768x86.png" width="156" height="18" />
 				<div>WHITE / SIN TÍTULO</div>
 				<div className="icons-container">
 					<div className="icon-container">
