95d88f4e580c9b8474596be66e4e4ab4ee127def
JoabMedel <joab_medel99@hotmail.com>
2022-11-22 17:05:32 -0600

    change rsponsive sideBar

--- src/core/layout/dashboard/sideBar/SideBar.jsx
+++ src/core/layout/dashboard/sideBar/SideBar.jsx
@@ -8,15 +8,19 @@ import "./SideBar.scss";
 const SideBar = () => {
 	const [ isfullSize, setIsFullSize ] = useState(false);
 
-	// const isAvailableDocs = true;
+	const isAvailableDocs = true;
 
 	return (
-		<div id="SideBar" className={isfullSize && "isFullSize"}>
-			<div className={`action-sidebar-conatiner ${isfullSize && "isFullSize"}`}>
-				<ArrowTop size="18px" className="icon-arrow-action isFullSize" onClick={() => setIsFullSize(!isfullSize)} />
-				<DropFile size="18px" />
-				<FolderPlus size="20px" />
-			</div>
+		<div id="SideBar" className={isAvailableDocs ? (isfullSize && "isFullSize") : "isNoData"}>
+			{
+				isAvailableDocs && (
+					<div className={`action-sidebar-conatiner ${isfullSize && "isFullSize"}`}>
+						<ArrowTop size="18px" className="icon-arrow-action isFullSize" onClick={() => setIsFullSize(!isfullSize)} />
+						<DropFile size="18px" />
+						<FolderPlus size="20px" />
+					</div>
+				)
+			}
 			<div className="body-sidebar">
 				<BodyGallery />
 			</div>
