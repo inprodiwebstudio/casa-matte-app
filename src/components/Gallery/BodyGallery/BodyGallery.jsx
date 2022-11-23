20e72b69511f3012e94b2550603d5a460a2b1ff9
JoabMedel <joab_medel99@hotmail.com>
2022-11-22 18:25:36 -0600

    resolved new changes responsive

--- src/components/Gallery/BodyGallery/BodyGallery.jsx
+++ src/components/Gallery/BodyGallery/BodyGallery.jsx
@@ -5,13 +5,101 @@ import DropDoc from "../DropDoc";
 import "./BodyGallery.scss";
 
 const BodyGallery = () => {
+	const isAvailableDocs = true;
 	return (
 		<div className="BodyGallery">
 			<div className="header-gallery-container">
 				<div style={{ height : "26px", width : "100%" }}>&nbsp;</div>
 				<h3>GALERÍA</h3>
 			</div>
-			<DropDoc />
+			{
+				!isAvailableDocs && (
+					<DropDoc />
+				)
+			}
+			<div className="photo-grid">
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+				<div
+					style={{
+						background : "grey",
+						width      : "157px",
+						height     : "157px",
+					}}
+				>
+					&nbsp;
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/core/layout/dashboard/sideBar/SideBar.jsx
+++ src/core/layout/dashboard/sideBar/SideBar.jsx
@@ -8,7 +8,7 @@ import "./SideBar.scss";
 const SideBar = () => {
 	const [ isfullSize, setIsFullSize ] = useState(false);
 
-	const isAvailableDocs = false;
+	const isAvailableDocs = true;
 
 	return (
 		<div id="SideBar" className={isAvailableDocs ? (isfullSize && "isFullSize") : "isNoData"}>
