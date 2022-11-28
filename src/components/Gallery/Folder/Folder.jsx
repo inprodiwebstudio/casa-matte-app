9283afb99c4088cc59e2008b6226744b1d3e784d
JoabMedel <joab_medel99@hotmail.com>
2022-11-28 13:35:47 -0600

    folder end

--- src/components/Gallery/Folder/Folder.jsx
+++ src/components/Gallery/Folder/Folder.jsx
@@ -1,8 +1,15 @@
+import { useState }     from "react";
+import { isValidArray } from "helpers";
+
 //Own omponents
 import { MoreOption, PlusIcon } from "Resources/icons";
+import imageTest                from "Resources/images/testingImage01.jpg";
+
 import "./Folder.scss";
 
 const Folder = () => {
+	const [ myImagesList, setMyImagesList ] = useState([]);
+
 	return (
 		<div className="Folder">
 			<div className="header-folder">
@@ -11,12 +18,47 @@ const Folder = () => {
 					<MoreOption size="20px" />
 				</div>
 			</div>
-			<div className="drager-body-container">
-				<div className="body-container">
-					<PlusIcon size="20px" />
-					<p className="text-container">
-						ARRASTRA AQUÍ TUS FOTOGRAFÍAS
-					</p>
+			<div className="body-indicator-conatiner">
+				<div className="photo-thumb-nail-container">
+					<div
+						className={`photo-indicator ${myImagesList[0] && "full-size"}`}
+						style={{
+							backgroundImage : myImagesList[0] ? `url(${myImagesList[0]?.image})` : null,
+						}}
+					/>
+					<div
+						className={`photo-indicator ${myImagesList[1] && "full-size"}`}
+						style={{
+							backgroundImage : myImagesList[1] ? `url(${myImagesList[1]?.image})` : null,
+						}}
+					/>
+					<div
+						className={`photo-indicator ${myImagesList[2] && "full-size"}`}
+						style={{
+							backgroundImage : myImagesList[2] ? `url(${myImagesList[2]?.image})` : null,
+						}}
+					/>
+					<div
+						className={`photo-indicator ${myImagesList[3] && "full-size"}`}
+						style={{
+							backgroundImage : myImagesList[3] ? `url(${myImagesList[3]?.image})` : null,
+						}}
+					/>
+				</div>
+				<div
+					className="drager-place"
+					onClick={() => setMyImagesList([...myImagesList, {image : imageTest}])}
+				>
+					<div className="label-indicator-drager">
+						<PlusIcon size="20px" />
+						{
+							!isValidArray(myImagesList) && (
+								<p>
+									AGREGAR FOTOS
+								</p>
+							)
+						}
+					</div>
 				</div>
 			</div>
 		</div>
