e69195ad749834d5dd26edb734b5f20c702baa5f
JoabMedel <joab_medel99@hotmail.com>
2023-01-02 18:12:36 -0600

    edit pages in workSpace

--- src/components/BookPages/LargeFormat/LargeFormat.jsx
+++ src/components/BookPages/LargeFormat/LargeFormat.jsx
@@ -4,9 +4,13 @@ import "./LargeFormat.scss";
 const LargeFormat = () => {
 	return (
 		<div className="LargeFormat">
-			<div className="page-body">page1</div>
-			<div className="spacer-page" />
-			<div className="page-body">page2</div>
+			<div className="page-body">
+				<div style={{
+					width  : "calc(200% + 4px)",
+					height : "100%",
+				}} />
+			</div>
+			<div className="page-body"></div>
 		</div>
 	);
 };
