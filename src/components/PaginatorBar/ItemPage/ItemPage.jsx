43d0962ba335c826832fa5b3b6118acdd13ac00f
JoabMedel <joab_medel99@hotmail.com>
2023-01-16 16:18:37 -0600

    pages navigate

--- src/components/PaginatorBar/ItemPage/ItemPage.jsx
+++ src/components/PaginatorBar/ItemPage/ItemPage.jsx
@@ -19,8 +19,6 @@ const ItemPage = ({
 
 	const isCurrentPage = pageId === draggableId;
 
-	console.log(pageId);
-
 	return (
 		<Draggable
 			draggableId={draggableId}
--- src/pages/dashboard/WorkSpace/WorkSpace.jsx
+++ src/pages/dashboard/WorkSpace/WorkSpace.jsx
@@ -1,29 +1,37 @@
+import { connect }   from "react-redux";
+import { useParams } from "react-router-dom";
+
 //Own components
 import FormatPage from "components/FormatPage";
 import "./WorkSpace.scss";
 
-const WorkSpace = () => {
+const WorkSpace = ({ workSpaceData }) => {
+	const { pageId } = useParams();
+	const myWorkSpaceData = workSpaceData[pageId];
+
+	console.log(myWorkSpaceData);
 	return (
 		<div className="WorkSpace">
 			<div className="canva-space">
 				<div className="ghost-canva">
-					<FormatPage
-						typeFormat="LargeFormat"
-						pageData={{
-							id     : "FrontLayout",
-							sheet1 : {
-								layoutType : "FrontLayout",
-								text       : "",
-								photos     : {
-									1 : "",
-								},
-							},
-						}}
-					/>
+					{
+						myWorkSpaceData && (
+							<FormatPage
+								typeFormat="LargeFormat"
+								pageData={{
+									...myWorkSpaceData,
+								}}
+							/>
+						)
+					}
 				</div>
 			</div>
 		</div>
 	);
 };
 
-export default WorkSpace;
+const mapStateToProps = ({ workSpaceSlice }) => ({
+	workSpaceData : workSpaceSlice?.data?.pages ?? {},
+});
+
+export default connect(mapStateToProps) (WorkSpace);
