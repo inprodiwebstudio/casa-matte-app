bb29747e9a2cc4a76d4b6cdb51583b4b67d08fae
JoabMedel <joab_medel99@hotmail.com>
2023-01-24 18:05:18 -0600

    resolved add photo editor

--- src/components/Modals/EditPhoto/EditPhoto.jsx
+++ src/components/Modals/EditPhoto/EditPhoto.jsx
@@ -1,6 +1,37 @@
+//React FileRobotEditor
+import FilerobotImageEditor, {
+	TABS,
+} from "react-filerobot-image-editor";
+
 const EditPhoto = () => {
 	return (
-		<div>Hola mundo</div>
+		<div>
+			<FilerobotImageEditor
+				source="https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg"
+				onSave={(editedImageObject, designState) =>
+					console.log("saved", editedImageObject, designState)}
+				// onClose={closeImgEditor}
+				annotationsCommon={{
+					fill : "#bb3214",
+				}}
+				// theme={{
+				//   palette: {
+				//     "accent-primary": "blue",
+				//     "bg-primary-active": "blue",
+				//     "accent-primary-hover": "#da5"
+				//   },
+				//   typography: {
+				//     fontFamily: "Helvetica"
+				//   }
+				// }}
+				language="es"
+				Text={{ text : "Filerobot..." }}
+				Rotate={{ angle : 90, componentType : "slider" }}
+				tabsIds={[TABS.ADJUST, TABS.FILTERS, TABS.FINETUNE]} // or {['Adjust', 'Annotate', 'Watermark']}
+				defaultTabId={TABS.ADJUST} // or 'Annotate'
+				// defaultToolId={TOOLS.TEXT} // or 'Text'
+			/>
+		</div>
 	);
 };
 
