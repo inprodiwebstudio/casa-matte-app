6713712b8923a9839e4b41165d8b0af3df51d7c7
JoabMedel <joab_medel99@hotmail.com>
2023-02-03 17:08:47 -0600

    fix(modsconstants): resizer image
    Change sieze minum quality image in thumbnails paginator
    
    BREAKING CHANGE: Resizer global functionality

--- src/components/global/LayoutsPage/ModsConstants.jsx
+++ src/components/global/LayoutsPage/ModsConstants.jsx
@@ -4,5 +4,5 @@ export const handlerResizerImage = (images, index, isInWorkSpcae) => {
 	if (isInWorkSpcae) {
 		return resizerImage(images[index], 900, 600);
 	}
-	return resizerImage(images[index], 100, 100);
+	return resizerImage(images[index], 150, 150);
 };
