4475d20e896dbc8f010a0b4ffe5d4dc8c614f70a
JoabMedel <joab_medel99@hotmail.com>
2023-02-24 17:11:11 -0600

    resolved new changes

--- src/components/global/LayoutsPage/ModsConstants.jsx
+++ src/components/global/LayoutsPage/ModsConstants.jsx
@@ -4,5 +4,5 @@ export const handlerResizerImage = (images, index, isInWorkSpcae) => {
 	if (isInWorkSpcae) {
 		return resizerImage(images[index].url, 900, 600, null);
 	}
-	return resizerImage(images[index].url, 150, 150);
+	return resizerImage(images[index].url, 100, 100);
 };
