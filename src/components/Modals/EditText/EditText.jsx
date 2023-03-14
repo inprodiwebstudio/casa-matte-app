d75291956b35d4560a132d5b469e55876d3e2716
JoabMedel <joab_medel99@hotmail.com>
2023-03-14 14:25:59 -0600

    resolved new changes

--- src/components/Modals/EditText/EditText.jsx
+++ src/components/Modals/EditText/EditText.jsx
@@ -1,14 +1,17 @@
-// import { Editor }  from "react-draft-wysiwyg";
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
 // import draftToHtml from "draftjs-to-html";
 // import htmlToDraft from "html-to-draftjs";
-// import {CKEditor}    from "@ckeditor/ckeditor5-react";
-// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor }  from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials    from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Bold          from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic        from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Paragraph     from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import FontFamily    from "@ckeditor/ckeditor5-font/src/fontfamily";
-// import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
-// import Font          from "@ckeditor/ckeditor5-font/src/font";
-// import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+import FontColor     from "@ckeditor/ckeditor5-font/src/fontcolor";
+import FontSize      from "@ckeditor/ckeditor5-font/src/fontsize";
+import Alignment     from "@ckeditor/ckeditor5-alignment/src/alignment";
 import "@ckeditor/ckeditor5-build-classic/build/translations/es";
 
 // import { EditorState, convertToRaw, ContentState } from "draft-js";
@@ -22,7 +25,6 @@ import { Button }  from "core/components";
 import { Check }   from "Resources/icons";
 import { bindAll } from "helpers";
 import "./EditText.scss";
-// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const EditText = () => {
 	// const handleEditorChange = (event, editor) => {
@@ -52,44 +54,93 @@ const EditText = () => {
 	// 	}
 	// }, [dataTextPage]);
 
+	const editorConfiguration = {
+		plugins   : [ Essentials, Bold, Alignment, Italic, Paragraph, FontFamily, FontSize, FontColor],
+		alignment : {
+			options : [ "left", "right", "center", "justify" ],
+		},
+		fontFamily : {
+			options : [
+				"default",
+				"Arial, Helvetica, sans-serif",
+				"Courier New, Courier, monospace",
+				"Georgia, serif",
+				"Lucida Sans Unicode, Lucida Grande, sans-serif",
+				"Tahoma, Geneva, sans-serif",
+				"Times New Roman, Times, serif",
+				"Trebuchet MS, Helvetica, sans-serif",
+				"Verdana, Geneva, sans-serif",
+				"BlakaHollow-Regular",
+				"Aitana-Regular",
+			],
+		},
+		toolbar : [
+			"fontfamily",
+			"fontSize",
+			 "|",
+			"bold",
+			"italic",
+			"|",
+			"fontColor",
+			"|",
+			"alignment:left",
+			"alignment:right",
+			"alignment:center",
+			"alignment:justify",
+			"|",
+			"undo",
+			"redo",
+		],
+		language : "es",
+		tooltip  : {
+			isRendered : false,
+		},
+		fontColor : {
+			colors : [
+				{
+					color : "hsl(0, 0%, 0%)",
+					label : "Black",
+				},
+				{
+					color : "hsl(0, 0%, 30%)",
+					label : "Dim grey",
+				},
+				{
+					color : "hsl(0, 0%, 60%)",
+					label : "Grey",
+				},
+				{
+					color : "hsl(0, 0%, 90%)",
+					label : "Light grey",
+				},
+				{
+					color     : "hsl(0, 0%, 100%)",
+					label     : "White",
+					hasBorder : true,
+				},
+			],
+		},
+		fontSize : {
+			options : [
+				9,
+				11,
+				13,
+				17,
+				19,
+				21,
+			],
+			supportAllValues : true,
+		},
+	};
+
 	return (
 		<div className="EditText">
 			<CKEditor
 				editor={ ClassicEditor }
+				config={ editorConfiguration }
 				data="<p>Hello from CKEditor 5!</p>"
-				// onReady={(editor) => {
-				// 	// Add the font family plugin to the editor
-				// 	editor.plugins.get("FontFamily").options.options = [
-				// 	  "default",
-				// 	  "Arial, sans-serif",
-				// 	  "Courier New, Courier, monospace",
-				// 	  "Georgia, serif",
-				// 	  "Lucida Sans Unicode, Lucida Grande, sans-serif",
-				// 	  "Tahoma, Geneva, sans-serif",
-				// 	  "Times New Roman, Times, serif",
-				// 	  "Trebuchet MS, Helvetica, sans-serif",
-				// 	  "Verdana, Geneva, sans-serif",
-				// 	];
-				// }}
-				config={ {
-					language : "es",
-					plugins  : [FontFamily],
-					toolbar  : ["fontfamily"],
-					// fontFamily : {
-					// 	options : [
-					// 	  "default",
-					// 	  "Ubuntu, Arial, sans-serif",
-					// 	  "Courier New, Courier, monospace",
-					// 	  "Georgia, serif",
-					// 	  "Lucida Sans Unicode, Lucida Grande, sans-serif",
-					// 	  "Tahoma, Geneva, sans-serif",
-					// 	  "Verdana, Geneva, sans-serif",
-					// 	],
-					// },
-				} }
 				onReady={ editor => {
-					// You can store the "editor" and use when it is needed.
-					console.log( "Editor is ready to use!", editor );
+					console.log(editor);
 				} }
 				onChange={ ( event, editor ) => {
 					const data = editor.getData();
@@ -102,47 +153,6 @@ const EditText = () => {
 					console.log( "Focus.", editor );
 				} }
 			/>
-			{/* <Editor
-				editorState={editorState}
-				toolbarClassName="toolbarClassName"
-				wrapperClassName="wrapperClassName"
-				editorClassName="editorClassName"
-				onEditorStateChange={onEditorStateChange}
-				toolbar={{
-					options : ["fontFamily", "fontSize", "colorPicker", "inline", "textAlign", "history"],
-					inline  : {
-						options : ["bold"],
-						bold    : {
-							className : "inline-styled",
-						},
-					},
-					fontFamily : {
-						options           : ["Helvetica", "Georgia", "Tahoma", "BlakaHollow-Regular"],
-						className         : "font-family-input",
-						component         : undefined,
-						dropdownClassName : "font-family-dropDown",
-					},
-					fontSize : {
-						className         : "font-size-selector",
-						dropdownClassName : "font-size-dropdown",
-					},
-					colorPicker : {
-						className      : "color-picker-editor",
-						popupClassName : "popup-styled-picker",
-					},
-					textAlign : {
-						left    : { className : "text-aling-styled" },
-						center  : { className : "text-aling-styled" },
-						right   : { className : "text-aling-styled" },
-						justify : { className : "text-aling-styled" },
-					},
-					history : {
-						className : "history-styled",
-						undo      : { className : "history-undo" },
-						redo      : { className : "history-redo" },
-					},
-				}}
-			/> */}
 			<div className="button-container">
 				<Button
 					icon={<Check size="15px" />}
