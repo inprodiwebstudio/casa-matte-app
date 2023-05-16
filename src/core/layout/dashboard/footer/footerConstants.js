9e3d3c7aef767d417085e122ae580ebbd08cef4a
JoabMedel <joab_medel99@hotmail.com>
2023-05-16 13:17:23 -0600

    refactor(footerconstants.js): create a constant file
    Create constants file footer app component

---
+++ src/core/layout/dashboard/footer/footerConstants.js
@@ -0,0 +1,61 @@
+export const filterTabs = [
+	{
+		label  : "TODOS",
+		filter : "all",
+	},
+	{
+		label  : "SOLO FOTOS",
+		filter : "fotos",
+	},
+	{
+		label  : "SOLO TEXTO",
+		filter : "texto",
+	},
+	{
+		label  : "FOTOS Y TEXTO",
+		filter : "fotosytexto",
+	},
+];
+
+export const optionsPhotoQuantity = [
+	{
+		label : "1 Foto",
+		value : "1 Foto",
+	},
+	{
+		label : "2 Fotos",
+		value : "2 Fotos",
+	},
+	{
+		label : "3 Fotos",
+		value : "3 Fotos",
+	},
+	{
+		label : "4 Fotos",
+		value : "4 Fotos",
+	},
+	{
+		label : "5 Fotos",
+		value : "5 Fotos",
+	},
+	{
+		label : "6 Fotos",
+		value : "6 Fotos",
+	},
+	{
+		label : "7 Fotos",
+		value : "7 Fotos",
+	},
+	{
+		label : "8 Fotos",
+		value : "8 Fotos",
+	},
+	{
+		label : "9 Fotos",
+		value : "9 Fotos",
+	},
+	{
+		label : "Todos",
+		value : "Todos",
+	},
+];
