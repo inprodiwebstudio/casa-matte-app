45a08871b0be282845e39c21ee6dcf257b2ece60
JoabMedel <joab_medel99@hotmail.com>
2023-05-29 16:09:42 -0600

    feat: handle loading and apply styles

--- src/core/components/Tabs/Tabs.jsx
+++ src/core/components/Tabs/Tabs.jsx
@@ -2,16 +2,19 @@ import { useState }                               from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 
 //Own components
+import { Loading }        from "core/components";
 import { workSpaceSlice } from "store/Slices";
 
 import "./Tabs.scss";
 
-const Tabs = ({tabList}) => {
+const Tabs = ({tabList, loading}) => {
 	const [ currentTab, setCurrentTab ] = useState(tabList[0].label);
 
 	const dispatch = useDispatch();
 
 	const currentFileterLayout = useSelector((state) => state.workSpaceSlice.layoutFilter, shallowEqual);
+	const isLoggin = useSelector((state) => state.authSlice.loggedIn, shallowEqual);
+
 
 	const onActionTab = (tabName, filterName) => {
 		dispatch(workSpaceSlice.actions.setLayoutFilter({
@@ -24,17 +27,21 @@ const Tabs = ({tabList}) => {
 	return (
 		<div className="Tabs">
 			{
-				tabList.map((tab, index) => (
-					<div
-						className={`box-tab ${(currentTab === tab.label) && "active"}`}
-						onClick={() => onActionTab(tab.label, tab.filter)}
-						key={index}
-					>
-						<h4>
-							{tab.label}
-						</h4>
-					</div>
-				))
+				(loading || !isLoggin) ? (
+					<Loading />
+				) : (
+					tabList.map((tab, index) => (
+						<div
+							className={`box-tab ${(currentTab === tab.label) && "active"}`}
+							onClick={() => onActionTab(tab.label, tab.filter)}
+							key={index}
+						>
+							<h4>
+								{tab.label}
+							</h4>
+						</div>
+					))
+				)
 			}
 		</div>
 	);
