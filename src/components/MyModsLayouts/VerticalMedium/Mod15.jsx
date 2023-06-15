2af729184b2173caaa65abc6d4935322acb9eab7
JoabMedel <joab_medel99@hotmail.com>
2023-06-15 09:16:52 -0600

    fix: change name folder

---
+++ src/components/MyModsLayouts/VerticalMedium/Mod15.jsx
@@ -0,0 +1,64 @@
+import { Stack, Flex } from "@mantine/core";
+//Own components
+import ImgLayout from "components/LayoutHandler/ImgLayout";
+
+const Mod15 = ({data, isInWorkSpace, sheetNo}) => {
+	return (
+		<Stack
+			w="100%"
+			h="100%"
+			p="3.5%"
+			pl="4.5%"
+			pr="4.5%"
+		>
+			<Flex w="100%" h="100%" gap="0.05em">
+				<Stack w="61%" h="100%" spacing="0.05em">
+					<Stack h="33.33%" w="100%">
+						<ImgLayout
+							isInWorkSpace={isInWorkSpace}
+							sheetNo={sheetNo}
+							imageNo={0}
+							urlImage={data?.photos[0] ?? {}}
+						/>
+					</Stack>
+					<Stack h="33.33%" w="100%">
+						<ImgLayout
+							isInWorkSpace={isInWorkSpace}
+							sheetNo={sheetNo}
+							imageNo={1}
+							urlImage={data?.photos[1] ?? {}}
+						/>
+					</Stack>
+					<Stack h="33.33%" w="100%">
+						<ImgLayout
+							isInWorkSpace={isInWorkSpace}
+							sheetNo={sheetNo}
+							imageNo={2}
+							urlImage={data?.photos[2] ?? {}}
+						/>
+					</Stack>
+				</Stack>
+				<Stack w="39%" h="100%" spacing="0.05em">
+					<Stack h="50%" w="100%">
+						<ImgLayout
+							isInWorkSpace={isInWorkSpace}
+							sheetNo={sheetNo}
+							imageNo={3}
+							urlImage={data?.photos[3] ?? {}}
+						/>
+					</Stack>
+					<Stack h="50%" w="100%">
+						<ImgLayout
+							isInWorkSpace={isInWorkSpace}
+							sheetNo={sheetNo}
+							imageNo={4}
+							urlImage={data?.photos[4] ?? {}}
+						/>
+					</Stack>
+				</Stack>
+			</Flex>
+		</Stack>
+	);
+};
+
+export default Mod15;
---
+++ src/components/MyModsLayouts/VerticalMedium/Mod3.jsx
@@ -0,0 +1,24 @@
+import { Stack } from "@mantine/core";
+//Own components
+import ImgLayout from "components/LayoutHandler/ImgLayout";
+
+const Mod3 = ({data, isInWorkSpace, sheetNo}) => {
+	return (
+		<Stack
+			w="100%"
+			h="100%"
+			p="19%"
+			pr="22%"
+			pl="22%"
+		>
+			<ImgLayout
+				isInWorkSpace={isInWorkSpace}
+				sheetNo={sheetNo}
+				imageNo={0}
+				urlImage={data?.photos[0] ?? {}}
+			/>
+		</Stack>
+	);
+};
+
+export default Mod3;
