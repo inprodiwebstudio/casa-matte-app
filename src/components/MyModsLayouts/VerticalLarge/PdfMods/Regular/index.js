7ffdf8ea1ce00ffea2c9805c505f5d428ed5e54a
JoabMedel <joab_medel99@hotmail.com>
2024-11-15 15:47:20 -0600

    change import mod32Pdf

--- src/components/MyModsLayouts/VerticalLarge/PdfMods/Regular/index.js
+++ src/components/MyModsLayouts/VerticalLarge/PdfMods/Regular/index.js
@@ -29,7 +29,7 @@ export { default as Mod28Pdf} from "./Mod28Pdf";
 export { default as Mod29Pdf} from "./Mod29Pdf";
 export { default as Mod30Pdf} from "./Mod30Pdf";
 export { default as Mod31Pdf} from "./Mod31Pdf";
-export { default as Mod32Pdf} from "./Mod32Pdf";
+export { default as Mod32Pdf} from "./Mod32pdf";
 export { default as Mod33Pdf} from "./Mod33Pdf";
 export { default as Mod34Pdf} from "./Mod34Pdf";
 export { default as Mod35Pdf} from "./Mod35Pdf";
