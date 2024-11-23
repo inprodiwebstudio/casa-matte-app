6c51e22c0276b6c7f8e9303db1adfd838b0a7a9a
JoabMedel <joab_medel99@hotmail.com>
2024-11-22 21:58:41 -0600

    fix delete tiemesTamp to send photos

--- src/helpers/Hooks/useSubmitImages.jsx
+++ src/helpers/Hooks/useSubmitImages.jsx
@@ -4,14 +4,14 @@ import axios           from "axios";
 const { useGenerateSignMutation } = apiImageKit;
 
 const useSubmitImages = ({userName, folderName}) => {
-	const timestamp = Math.floor(Date.now() / 1000);
+	// const timestamp = Math.floor(Date.now() / 1000);
 	const [ generateSignMutation ] = useGenerateSignMutation();
 	const [ generateUrlCompress ] = apiImageKit.useGenerateUrlCompressMutation();
 	const handlerUploadImage = async (image, isditedPhoto) => {
 		try {
 			const { data } = await generateSignMutation({data : {
-				timestamp : timestamp,
-				folder    : `${userName}/${folderName ? folderName : isditedPhoto ? "_editedPhotos" : ""}`,
+				// timestamp : timestamp,
+				folder : `${userName}/${folderName ? folderName : isditedPhoto ? "_editedPhotos" : ""}`,
 			}});
 			const uploadFile = await axios.postForm(
 				"https://api.cloudinary.com/v1_1/dtjvmtfji/image/upload",
