169f5e10f3bd7e9fd85bc085c7672d0bf59e2810
JoabMedel <joab_medel99@hotmail.com>
2024-11-25 17:33:37 -0600

    fix position text layouts

--- src/components/MyModsLayouts/VerticalLarge/PdfMods/Regular/Mod47Pdf.jsx
+++ src/components/MyModsLayouts/VerticalLarge/PdfMods/Regular/Mod47Pdf.jsx
@@ -11,13 +11,13 @@ const Mod47Pdf = ({text}) => {
 
 	const text01 = text[0] ? text[0] : "<p style='text-align: left;'><span style='font-size: 24px; font-family: JosefinSans-Light;'>ESPAÑA</span></p>";
 
-	const text02 = text[1] ? text[1] : "<p style='text-align: left;'><span style='font-size: 24px; font-family: JosefinSans-Light;'>FRANCIA</span></p>";
+	const text02 = text[2] ? text[2] : "<p style='text-align: left;'><span style='font-size: 24px; font-family: JosefinSans-Light;'>FRANCIA</span></p>";
 
-	const text03 = text[2] ? text[2] : "<p style='text-align: left;'><span style='font-size: 24px; font-family: JosefinSans-Light;'>TURQUÍA</span></p>";
+	const text03 = text[4] ? text[4] : "<p style='text-align: left;'><span style='font-size: 24px; font-family: JosefinSans-Light;'>TURQUÍA</span></p>";
 
-	const text04 = text[3] ? text[3] : "<p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Madrid</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Segovia</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Salamanca</span></p>";
+	const text04 = text[1] ? text[1] : "<p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Madrid</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Segovia</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Salamanca</span></p>";
 
-	const text05 = text[4] ? text[4] : "<p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>París</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Versalles</span></p>";
+	const text05 = text[3] ? text[3] : "<p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>París</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Versalles</span></p>";
 
 	const text06 = text[5] ? text[5] : "<p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Estambul</span></p><p style='text-align: left;'><span style='font-size: 12px; font-family: Inter-Lifght;'>Capadocia</span></p>";
 
