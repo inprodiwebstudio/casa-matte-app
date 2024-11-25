76d88a4d4911ccfd97c9e06239d048762ad3444f
JoabMedel <joab_medel99@hotmail.com>
2024-11-25 16:16:57 -0600

    add redirect logo

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -51,7 +51,9 @@ const Header = () => {
 	return (
 		<div className="Header">
 			<div className={`body-container ${isPreviewActive && "isActivePreview"}`}>
-				<img src={LogoCasaMatte} width={120} />
+				<a href="https://casamatte.wip-inprodi.com/">
+					<img src={LogoCasaMatte} width={120} />
+				</a>
 				{
 					isLoggedIn && (
 						<>
