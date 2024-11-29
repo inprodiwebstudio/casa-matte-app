9055e9e6c14076040ce904845b526f6623261009
JoabMedel <joab_medel99@hotmail.com>
2024-11-29 17:16:52 -0600

    to resolved extra pages

--- src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
+++ src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
@@ -120,6 +120,11 @@ const ConfirmationToPrint = () => {
 				return extraCost;
 			};
 
+			if (extraPages) {
+				console.log(extraPages.toString());
+				return;
+			}
+
 			const responseCreateOrder = await axios.post(
 				"https://casamatte.com/wp-json/wc/v3/orders",
 				{
