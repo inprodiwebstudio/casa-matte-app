4dda1c7502ffca869c2c04f0695652fe636f9b8f
JoabMedel <joab_medel99@hotmail.com>
2025-01-08 09:51:07 -0600

    chage path consume

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,7 +1,7 @@
 const isProduction = import.meta.env.PROD;
 
 const apiUrl = isProduction
-	? import.meta.env.VITE_API
+	? "https://casa-matte-api-cs6c4.ondigitalocean.app/api/v1/"
 	: import.meta.env.VITE_API_LOCAL;
 
 export default apiUrl;
