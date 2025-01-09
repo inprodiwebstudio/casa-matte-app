3b57779f5526497d08e15ea8238ed5f3bcbf8203
JoabMedel <joab_medel99@hotmail.com>
2025-01-09 11:22:24 -0600

    parse string prev

--- src/pages/PayConfirm/PayConfirm.jsx
+++ src/pages/PayConfirm/PayConfirm.jsx
@@ -229,7 +229,8 @@ const PayConfirm = () => {
 		// }
 			if (photobookData?.meta?.config) {
 				const myData = photobookData?.meta?.config;
-				const parseJSON = JSON.parse(myData);
+				const myReplacerString = myData.replace(/'/g, "\"");
+				const parseJSON = JSON.parse(myReplacerString);
 				dispatch(workSpaceSlice.actions.insertData({...parseJSON, modified : photobookData?.modified, projectTittle : photobookData?.title?.rendered}));
 			}
 		} catch (error) {
