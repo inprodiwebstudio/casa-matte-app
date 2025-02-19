e5b17f8626d3d413f5aa0f4b9184970e18b1431a
JoabMedel <joab_medel99@hotmail.com>
2025-02-19 13:43:36 -0600

    add permissions to view admin

--- src/core/components/AppShell/AppShell.jsx
+++ src/core/components/AppShell/AppShell.jsx
@@ -404,10 +404,10 @@ const AppShell = ({
 		// 	navigate("/notfound/layouts");
 		// 	return;
 		// }
-		// if (photobookData?.meta?.status === "48") {
-		// 	navigate(`/payment/confirm?orderid=${photobookData?.meta?.id_del_pedido}&postId=${postIdphotoBook}`);
-		// 	return;
-		// }
+		if ((photobookData?.meta?.status === "48") && (userName !== "casamatteadmin")) {
+			navigate(`/payment/confirm?orderid=${photobookData?.meta?.id_del_pedido}&postId=${postIdphotoBook}`);
+			return;
+		}
 		if (!userId && photobookData?.author) {
 			dispatch(authSlice.actions.setUserId(photobookData?.author));
 		}
