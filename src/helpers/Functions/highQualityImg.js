2d4683ef063bc7f1aa84ff03e1af046b324d34f4
JoabMedel <joab_medel99@hotmail.com>
2025-03-04 07:49:45 -0600

    change resolution prev photos

--- src/helpers/Functions/highQualityImg.js
+++ src/helpers/Functions/highQualityImg.js
@@ -2,7 +2,7 @@ const constructorImg = (urlimg) => {
 	if (!urlimg) return;
 	const splitImage = urlimg.split("upload");
 
-	return `${splitImage[0]}upload/q_60/v1/${urlimg.split("/v1/")[1]}`;
+	return `${splitImage[0]}upload/w_1920/v1/${urlimg.split("/v1/")[1]}`;
 };
 
 export default constructorImg;
