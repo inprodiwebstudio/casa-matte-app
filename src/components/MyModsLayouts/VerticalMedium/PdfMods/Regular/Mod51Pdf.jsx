b37525222f013e1c04e49b2b40bd482947ad2b2d
JoabMedel <joab_medel99@hotmail.com>
2025-03-06 15:02:05 -0600

    modify large vertial mod pdf

--- src/components/MyModsLayouts/VerticalMedium/PdfMods/Regular/Mod51Pdf.jsx
+++ src/components/MyModsLayouts/VerticalMedium/PdfMods/Regular/Mod51Pdf.jsx
@@ -83,7 +83,7 @@ const Mod51Pdf = ({text, images}) => {
 									 text-align: center !important;
                                    }
                                  </style>
-                                 <p style="text-align:center;"><span style="font-family:Inter-Lifght;font-size:14px;text-align:center;">CAMELINAS</span></p><p style="text-align:center;"><span style="font-family:Inter-Lifght;font-size:14px;">Nadamos con una Manta Gigante y disfrutamos nuestra propia playita</span></p>`,
+                                 <p style="text-align:center;background-color:red;"><span style="font-family:Inter-Lifght;font-size:14px;text-align:center;background-color:yellow">CAMELINAS</span></p><p style="text-align:center;"><span style="font-family:Inter-Lifght;font-size:14px;">Nadamos con una Manta Gigante y disfrutamos nuestra propia playita</span></p>`,
 					}}
 				/>
 			</div>
