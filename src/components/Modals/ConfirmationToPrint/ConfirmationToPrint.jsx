256480cb40eeadc01a853e6d550e692957d015a5
JoabMedel <joab_medel99@hotmail.com>
2025-03-10 17:40:57 -0600

    to merge branch

--- src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
+++ src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
@@ -88,7 +88,7 @@ const ConfirmationToPrint = () => {
 		const data = dataShipping;
 		setOrderLoading(true);
 		try {
-			const productDataRes = await axios.get(`https://casamatte.wip-inprodi.com/wp-json/wc/v3/products?search=${encodeURIComponent(productNameKey)}`,
+			const productDataRes = await axios.get(`https://casamatte.com/wp-json/wc/v3/products?search=${encodeURIComponent(productNameKey)}`,
 				{
 					auth : {
 						username : "ck_ecf36082e00a4cfd16000f338e25073359b78df2",
@@ -122,7 +122,7 @@ const ConfirmationToPrint = () => {
 
 
 			const responseCreateOrder = await axios.post(
-				"https://casamatte.wip-inprodi.com/wp-json/wc/v3/orders",
+				"https://casamatte.com/wp-json/wc/v3/orders",
 				{
 					payment_method       : "bacs",
 					payment_method_title : "Direct Bank Transfer",
