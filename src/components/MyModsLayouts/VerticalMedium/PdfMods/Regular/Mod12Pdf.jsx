72a1f9879eaa6ec4ba088b0501839c080f2aa7d7
JoabMedel <joab_medel99@hotmail.com>
2025-04-22 10:20:43 -0600

    fix(mod12 vertical medium change): revert mod12 layout vertical Large pdf

--- src/components/MyModsLayouts/VerticalMedium/PdfMods/Regular/Mod12Pdf.jsx
+++ src/components/MyModsLayouts/VerticalMedium/PdfMods/Regular/Mod12Pdf.jsx
@@ -12,143 +12,41 @@ const Mod12Pdf = ({images}) => {
 	const bodyHtml = (
 		<div
 			style={{
-				height         : "832px",
-				width          : "652px",
-				display        : "flex",
-				justifyContent : "center",
-				alignItems     : "center",
-				position       : "relative",
+				height  : "792px",
+				width   : "100%",
+				padding : "20px",
 			}}
 		>
 			<div
 				style={{
-					position     : "absolute",
-					top          : "0",
-					left         : "0",
-					width        : "20px",
-					height       : "20px",
-					borderBottom : "1px solid black",
-					borderRight  : "1px solid black",
-				}}
-			>
-				&nbsp;
-			</div>
-			<div
-				style={{
-					position     : "absolute",
-					top          : "0",
-					left         : "632px",
-					width        : "20px",
-					height       : "20px",
-					borderBottom : "1px solid black",
-					borderLeft   : "1px solid black",
-				}}
-			>
-				&nbsp;
-			</div>
-			<div
-				style={{
-					position   : "absolute",
-					top        : "812px",
-					left       : "632px",
-					width      : "20px",
-					height     : "20px",
-					borderTop  : "1px solid black",
-					borderLeft : "1px solid black",
-				}}
-			>
-				&nbsp;
-			</div>
-			<div
-				style={{
-					position    : "absolute",
-					top         : "812px",
-					left        : "0",
-					width       : "20px",
-					height      : "20px",
-					borderTop   : "1px solid black",
-					borderRight : "1px solid black",
-				}}
-			>
-				&nbsp;
-			</div>
-			<div
-				style={{
-					height  : "792px",
-					width   : "612px",
-					padding : "20px",
+					height        : "100%",
+					width         : "100%",
+					display       : "flex",
+					flexDirection : "column",
+					gap           : "10px",
 				}}
 			>
 				<div
 					style={{
-						height        : "100%",
 						width         : "100%",
+						height        : "calc(50% - 5px)",
 						display       : "flex",
-						flexDirection : "column",
+						flexDirection : "row",
 						gap           : "10px",
 					}}
 				>
 					<div
 						style={{
-							width         : "100%",
-							height        : "calc(50% - 5px)",
-							display       : "flex",
-							flexDirection : "row",
-							gap           : "10px",
-						}}
-					>
-						<div
-							style={{
-								width    : "calac(50% - 5px)",
-								height   : "100%",
-								overflow : "hidden",
-							}}
-						>
-							{
-								images[0]?.url && (
-									<img
-										src={imgUrlPdf(images[0])}
-										alt="test"
-										style={{
-											height    : "100%",
-											objectFit : "cover",
-										}}
-									/>
-								)
-							}
-						</div>
-						<div
-							style={{
-								width    : "calac(50% - 5px)",
-								height   : "100%",
-								overflow : "hidden",
-							}}
-						>
-							{
-								images[1]?.url && (
-									<img
-										src={imgUrlPdf(images[1])}
-										alt="test"
-										style={{
-											height    : "100%",
-											objectFit : "cover",
-										}}
-									/>
-								)
-							}
-						</div>
-					</div>
-					<div
-						style={{
-							width    : "100%",
-							height   : "calc(50% - 5px)",
-							overflow : "hidden",
+							width      : "calac(50% - 5px)",
+							height     : "100%",
+							overflow   : "hidden",
+							background : "#E3E3E3",
 						}}
 					>
 						{
-							images[2]?.url && (
+							images[0]?.url && (
 								<img
-									src={imgUrlPdf(images[2])}
+									src={imgUrlPdf(images[0])}
 									alt="test"
 									style={{
 										height    : "100%",
@@ -158,6 +56,48 @@ const Mod12Pdf = ({images}) => {
 							)
 						}
 					</div>
+					<div
+						style={{
+							width      : "calac(50% - 5px)",
+							height     : "100%",
+							overflow   : "hidden",
+							background : "#E3E3E3",
+						}}
+					>
+						{
+							images[1]?.url && (
+								<img
+									src={imgUrlPdf(images[1])}
+									alt="test"
+									style={{
+										height    : "100%",
+										objectFit : "cover",
+									}}
+								/>
+							)
+						}
+					</div>
+				</div>
+				<div
+					style={{
+						width      : "100%",
+						height     : "calc(50% - 5px)",
+						background : "#E3E3E3",
+						overflow   : "hidden",
+					}}
+				>
+					{
+						images[2]?.url && (
+							<img
+								src={imgUrlPdf(images[2])}
+								alt="test"
+								style={{
+									height    : "100%",
+									objectFit : "cover",
+								}}
+							/>
+						)
+					}
 				</div>
 			</div>
 		</div>
