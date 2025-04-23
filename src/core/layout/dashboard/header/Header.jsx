349434f29996df83ece5e99cd0b335c3cb4db0c1
JoabMedel <joab_medel99@hotmail.com>
2025-04-23 10:27:20 -0600

    feat(header): change logo link redirect page casa matte list of dashboard

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -75,7 +75,7 @@ const Header = () => {
 	return (
 		<div className="Header">
 			<div className={`body-container ${isPreviewActive && "isActivePreview"}`}>
-				<a href="https://casamatte.wip-inprodi.com/">
+				<a href="https://casamatte.wip-inprodi.com/dashboard/">
 					<img src={LogoCasaMatte} width={120} />
 				</a>
 				{
