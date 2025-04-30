9f7b807f1420137723eff7aa8de1d1f3b0c6954e
JoabMedel <joab_medel99@hotmail.com>
2025-04-30 11:01:29 -0600

    feat(create preview pdf): add Create preview and download pdf

--- src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
+++ src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
@@ -18,10 +18,16 @@ import JSZip                   from "jszip";
 import SquareLarge             from "components/MyModsLayouts/SquareLarge";
 import TravelCoffeeTable       from "components/MyModsLayouts/TravelCoffeeTable";
 import { Document, Page, pdf } from "@react-pdf/renderer";
+import { useDispatch }         from "react-redux";
+import { workSpaceSlice }      from "store/Slices";
+import { openContextModal }    from "@mantine/modals";
 
 const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 	const [ photoBookConfigData, setPhotoBookConfigData ] = useState(undefined);
 	const [ isLoading, setIsLoading ] = useState(false);
+
+	const dispatch = useDispatch();
+
 
 	const photoBookTypes = {
 		vertical : {
@@ -84,6 +90,7 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 		const myData = photoBookData?.metas?.config[0];
 		const newData = myData.replace(".heic", ".png");
 		const parseJSON = JSON.parse(newData);
+		dispatch(workSpaceSlice.actions.insertData({...parseJSON}));
 		setPhotoBookConfigData({...parseJSON});
 	};
 
@@ -208,7 +215,12 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 		const urlPhotos = listOfPhotos(photoBookConfigData?.pages);
 		const imagesOk = await validateAllImages(urlPhotos);
 		if (imagesOk) {
-			await createPDFPhotoBook(photoBookConfigData);
+			openContextModal({
+				modal      : "testPdf",
+				innerProps : {
+					photoBookData : photoBookConfigData,
+				},
+			});
 			setIsLoading(false);
 			return;
 		}
@@ -347,7 +359,7 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 						rightIcon={<SaveIcom size="12px" />}
 						fullWidth
 					>
-						DESCARGAR
+						PREVISUALIZAR Y DESCARGAR
 					</Button>
 					<Button
 						color="darkCasaMatte.6"
