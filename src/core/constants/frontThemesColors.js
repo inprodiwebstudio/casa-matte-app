3a4877c55cffe6e2d3ca33218d13791c5f498877
JoabMedel <joab_medel99@hotmail.com>
2025-05-04 23:43:55 -0600

    feat(dictionary textures): create dictionary textures

---
+++ src/core/constants/frontThemesColors.js
@@ -0,0 +1,290 @@
+import telaTexture     from "/Resources/images/texturesFronts/tela_textura.jpg";
+import curlPielTexture from "/Resources/images/texturesFronts/curlpiel_textura.jpg";
+import papelTexture    from "/Resources/images/texturesFronts/papel_textura.jpg";
+
+const frontThemesTextures = {
+	BRIGHT : {
+		Amarillo : {
+			color      : "#F5CD21",
+			textureUrl : telaTexture,
+		},
+		Naranja : {
+			color      : "#F15E23",
+			textureUrl : telaTexture,
+		},
+		Rojo : {
+			color      : "#CB2B27",
+			textureUrl : telaTexture,
+		},
+		Rosa : {
+			color      : "#B83C5E",
+			textureUrl : telaTexture,
+		},
+		"Rosa claro" : {
+			color      : "#E79BBA",
+			textureUrl : telaTexture,
+		},
+		Lila : {
+			color      : "#626882",
+			textureUrl : telaTexture,
+		},
+		Azul : {
+			color      : "#2D556F",
+			textureUrl : telaTexture,
+		},
+		Turquesa : {
+			color      : "#558B9E",
+			textureUrl : telaTexture,
+		},
+		"Azul claro" : {
+			color      : "#95BFC8",
+			textureUrl : telaTexture,
+		},
+		"Verde amarillo" : {
+			color      : "#D6C872",
+			textureUrl : telaTexture,
+		},
+		"Verde perico" : {
+			color      : "#F15E23",
+			textureUrl : telaTexture,
+		},
+	},
+	NUDE : {
+		Crema : {
+			color      : "#D3CEC4",
+			textureUrl : telaTexture,
+		},
+		Arena : {
+			color      : "#B19984",
+			textureUrl : telaTexture,
+		},
+		Capuccino : {
+			color      : "#AC8B70",
+			textureUrl : telaTexture,
+		},
+		"Lino Warm" : {
+			color      : "#C6B9AC",
+			textureUrl : telaTexture,
+		},
+		"Lino Cold" : {
+			color      : "#E79BBA",
+			textureUrl : telaTexture,
+		},
+		"Lino Natural" : {
+			color      : "#C5C2BB",
+			textureUrl : telaTexture,
+		},
+		Chocolate : {
+			color      : "#47362B",
+			textureUrl : telaTexture,
+		},
+		"Verde olivo" : {
+			color      : "#4B402B",
+			textureUrl : telaTexture,
+		},
+		"Verde pino" : {
+			color      : "#2E3F46",
+			textureUrl : telaTexture,
+		},
+		"Verde botella" : {
+			color      : "#2C4234",
+			textureUrl : telaTexture,
+		},
+		Salvia : {
+			color      : "#91A892",
+			textureUrl : telaTexture,
+		},
+		Menta : {
+			color      : "#B7B7A3",
+			textureUrl : telaTexture,
+		},
+		"Gris claro" : {
+			color      : "#C1C3C0",
+			textureUrl : telaTexture,
+		},
+		"Gris obscuro" : {
+			color      : "#635D5B",
+			textureUrl : telaTexture,
+		},
+		Negro : {
+			color      : "#20201C",
+			textureUrl : telaTexture,
+		},
+		"Azul marino" : {
+			color      : "#2E4252",
+			textureUrl : telaTexture,
+		},
+		Vino : {
+			color      : "#5C292B",
+			textureUrl : telaTexture,
+		},
+		Ladrillo : {
+			color      : "#9F2B26",
+			textureUrl : telaTexture,
+		},
+		Blush : {
+			color      : "#C48475",
+			textureUrl : telaTexture,
+		},
+		Ocre : {
+			color      : "#A85B26",
+			textureUrl : telaTexture,
+		},
+	},
+	PAPEL : {
+		Amarillo : {
+			color      : "#FAC221",
+			textureUrl : papelTexture,
+		},
+		Naranja : {
+			color      : "#F6891F",
+			textureUrl : papelTexture,
+		},
+		Coral : {
+			color      : "#E74936",
+			textureUrl : papelTexture,
+		},
+		Rojo : {
+			color      : "#E05054",
+			textureUrl : papelTexture,
+		},
+		Rosa : {
+			color      : "#E15386",
+			textureUrl : papelTexture,
+		},
+		"Azul obscuro" : {
+			color      : "#415D77",
+			textureUrl : papelTexture,
+		},
+		Turquesa : {
+			color      : "#166481",
+			textureUrl : papelTexture,
+		},
+		"Azul claro" : {
+			color      : "#4B402B",
+			textureUrl : papelTexture,
+		},
+		"Verde botella" : {
+			color      : "#1E724E",
+			textureUrl : papelTexture,
+		},
+		"Verde militar" : {
+			color      : "#5F8255",
+			textureUrl : papelTexture,
+		},
+		Menta : {
+			color      : "#C9BE90",
+			textureUrl : papelTexture,
+		},
+		Capuccino : {
+			color      : "#A6865D",
+			textureUrl : papelTexture,
+		},
+		Chocolate : {
+			color      : "#735343",
+			textureUrl : papelTexture,
+		},
+		"Gris Obscuro" : {
+			color      : "#3D3B3C",
+			textureUrl : papelTexture,
+		},
+		"Gris claro" : {
+			color      : "#D6D6D6",
+			textureUrl : papelTexture,
+		},
+		Blanco : {
+			color      : "#E5E4E4",
+			textureUrl : papelTexture,
+		},
+	},
+	CURPIEL : {
+		Marfil : {
+			color      : "#F5CD21",
+			textureUrl : curlPielTexture,
+		},
+		Avena : {
+			color      : "#F15E23",
+			textureUrl : curlPielTexture,
+		},
+		Topo : {
+			color      : "#918575",
+			textureUrl : curlPielTexture,
+		},
+		"Gris claro" : {
+			color      : "#8D8E8A",
+			textureUrl : curlPielTexture,
+		},
+		"Girs obscuro" : {
+			color      : "#48443F",
+			textureUrl : curlPielTexture,
+		},
+		Negro : {
+			color      : "#232323",
+			textureUrl : curlPielTexture,
+		},
+		"Azul marino" : {
+			color      : "#28384E",
+			textureUrl : curlPielTexture,
+		},
+		Tabaco : {
+			color      : "#3A2519",
+			textureUrl : curlPielTexture,
+		},
+		Chocolate : {
+			color      : "#593E29",
+			textureUrl : curlPielTexture,
+		},
+		Arcilla : {
+			color      : "#532722",
+			textureUrl : curlPielTexture,
+		},
+		Ladrillo : {
+			color      : "#7A342A",
+			textureUrl : curlPielTexture,
+		},
+		Camello : {
+			color      : "#7E552C",
+			textureUrl : curlPielTexture,
+		},
+	},
+	"PIEL VEGANA" : {
+		Marfil : {
+			color      : "#A79E8C",
+			textureUrl : curlPielTexture,
+		},
+		Topo : {
+			color      : "#897D78",
+			textureUrl : curlPielTexture,
+		},
+		"Gris claro" : {
+			color      : "#8F8F8F",
+			textureUrl : curlPielTexture,
+		},
+		"Gris obscuro" : {
+			color      : "#65676E",
+			textureUrl : curlPielTexture,
+		},
+		Negro : {
+			color      : "#323232",
+			textureUrl : curlPielTexture,
+		},
+		Tabaco : {
+			color      : "#514A45",
+			textureUrl : curlPielTexture,
+		},
+		Chocolate : {
+			color      : "#593833",
+			textureUrl : curlPielTexture,
+		},
+		Camello : {
+			color      : "#845143",
+			textureUrl : curlPielTexture,
+		},
+		"Azul rey" : {
+			color      : "#475A6D",
+			textureUrl : curlPielTexture,
+		},
+	},
+};
+
+export default frontThemesTextures;
