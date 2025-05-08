8e1a162b473bb38c70e3d5a44e512a2946f51760
JoabMedel <joab_medel99@hotmail.com>
2025-05-08 16:51:14 -0600

    create config colors letter

---
+++ src/core/constants/engravingColors.js
@@ -0,0 +1,75 @@
+import Holografico  from "../../Resources/images/engravingTextures/1. holográfico.png";
+import Plata        from "../../Resources/images/engravingTextures/2. plata.png";
+import Dorado       from "../../Resources/images/engravingTextures/3. dorado.png";
+import Cobre        from "../../Resources/images/engravingTextures/4. cobre.png";
+import OroRosa      from "../../Resources/images/engravingTextures/5. oro rosa.png";
+import Rosa         from "../../Resources/images/engravingTextures/6. rosa.png";
+import Magenta      from "../../Resources/images/engravingTextures/7. mangenta.png";
+import Rojo         from "../../Resources/images/engravingTextures/8. rojo.png";
+import AzulRey      from "../../Resources/images/engravingTextures/9. azul rey.png";
+import Turquesa     from "../../Resources/images/engravingTextures/10. turquesa.png";
+import VerdeClaro   from "../../Resources/images/engravingTextures/11. verde claro.png";
+import Negro        from "../../Resources/images/engravingTextures/12. negro.png";
+import Blanco       from "../../Resources/images/engravingTextures/13. blanco.png";
+import Transparente from "../../Resources/images/engravingTextures/14. transparente.png";
+
+const engravingColors = {
+	Holográfico : {
+		color   : "#7FEFD5",
+		texture : Holografico,
+	},
+	Plata : {
+		color   : "#C0C0C0",
+		texture : Plata,
+	},
+	Dorado : {
+		color   : "#FFD700",
+		texture : Dorado,
+	},
+	Cobre : {
+		color   : "#B87333",
+		texture : Cobre,
+	},
+	"Oro rosa" : {
+		color   : "#E6C229",
+		texture : OroRosa,
+	},
+	Rosa : {
+		color   : "#FFC0CB",
+		texture : Rosa,
+	},
+	Magenta : {
+		color   : "#FF00FF",
+		texture : Magenta,
+	},
+	Rojo : {
+		color   : "#FF0000",
+		texture : Rojo,
+	},
+	"Azul rey" : {
+		color   : "#002366",
+		texture : AzulRey,
+	},
+	Turquesa : {
+		color   : "#40E0D0",
+		texture : Turquesa,
+	},
+	"Verde claro" : {
+		color   : "#90EE90",
+		texture : VerdeClaro,
+	},
+	Negro : {
+		color   : "#000000",
+		texture : Negro,
+	},
+	Blanco : {
+		color   : "#FFFFFF",
+		texture : Blanco,
+	},
+	Transparente : {
+		color   : "#FFFFFF00",
+		texture : Transparente,
+	},
+};
+
+export default engravingColors;
