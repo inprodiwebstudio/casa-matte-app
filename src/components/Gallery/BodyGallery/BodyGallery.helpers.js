6cffd7b9645c1f91208bbe0e8b5993e261005c97
JoabMedel <joab_medel99@hotmail.com>
2025-05-13 22:09:04 -0600

    change heic to png list of gallery

--- src/components/Gallery/BodyGallery/BodyGallery.helpers.js
+++ src/components/Gallery/BodyGallery/BodyGallery.helpers.js
@@ -1,4 +1,15 @@
 export const gallerySeparation = (galleryData, isFolder) => {
 	const newData = galleryData.filter(data => isFolder ? data?.type === "folder" : data?.type === "file");
+	if (!isFolder) {
+		const imagesConvertToPng = newData.map(image => {
+			return {
+				...image,
+				url          : image?.url.includes(".heic") ? image?.url.replace(".heic", ".jpg") : image?.url,
+				urlThumbnail : image?.urlThumbnail.includes(".heic") ? image?.urlThumbnail.replace(".heic", ".jpg") : image?.urlThumbnail,
+			};
+		});
+
+		return imagesConvertToPng.reverse();
+	}
 	return newData.reverse();
 };
