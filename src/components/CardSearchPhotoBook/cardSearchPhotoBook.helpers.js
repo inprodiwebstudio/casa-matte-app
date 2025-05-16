648195ece80eb343cfabedce902791ed39126516
Joab Medel <65578244+JoabMedel@users.noreply.github.com>
2025-05-15 19:28:23 -0600

    Development (#34)
    * create a new branch
    
    * List of orders
    
    * change size table height
    
    * Fix spinner charge in table admin
    
    * change envs paths
    
    * View in progress photoBook admin list
    
    * Change regex to parse photos change to jpg
    
    * add product layflat Complte and fix
    
    * fix helper heic to png
    
    * change heic to png list of gallery
    
    * fix BookPages isInDouble page handler
    
    * change text complete photoBook page
    
    * revert changes to not transform base64

--- src/components/CardSearchPhotoBook/cardSearchPhotoBook.helpers.js
+++ src/components/CardSearchPhotoBook/cardSearchPhotoBook.helpers.js
@@ -1,62 +1,22 @@
-import { isValidArray }  from "helpers";
 import highQualityImgUrl from "helpers/Functions/highQualityImg";
 
-export const fetchImageAsBase64WithRetry = async (url, maxAttempts = 5) => {
-	let attempts = 0;
+export const loadImageWithRetry = (url, maxAttempts = 5) => {
+	return new Promise((resolve, reject) => {
+		let attempts = 0;
 
-	while (attempts < maxAttempts) {
-		try {
-			const response = await fetch(url);
-			const blob = await response.blob();
+		const tryLoad = () => {
+			const img = new Image();
+			img.src = highQualityImgUrl(url);
 
-			const base64 = await new Promise((resolve, reject) => {
-				const reader = new FileReader();
-				reader.onloadend = () => resolve(reader.result);
-				reader.onerror = reject;
-				reader.readAsDataURL(blob);
-			});
-
-			return base64;
-		} catch (err) {
-			attempts++;
-			if (attempts >= maxAttempts) {
-				throw new Error(`No se pudo cargar la imagen: ${url}`);
-			}
-			await new Promise((res) => setTimeout(res, 500));
-		}
-	}
-};
-
-export const subsTarctImagesInPagesData = (objPages) => {
-	if (!objPages) return new Error("Not pass pages data");
-
-	const pages = Object.values(objPages);
-
-	if (!isValidArray(pages)) return new Error("Error pages. Not valid array pages");
-
-	const listOfImagesInPhotoBook = [];
-
-	pages.forEach((page) => {
-		const pageId = page.id;
-		const { sheet2 } = page;
-
-		const imagesInSheet = (sheetNo) => {
-			if (page[`sheet${sheetNo}`].layoutType === "") return;
-			const sheetKey = `sheet${sheetNo}`;
-			const photos = page[sheetKey]?.photos;
-			const photosArray = Object.values(photos);
-			if (isValidArray(photosArray)) {
-				photosArray.forEach((photo, index) => {
-					if (photo.id !== "") {
-						listOfImagesInPhotoBook.push({
-							pageId,
-							sheetKey,
-							photoNo  : index,
-							imageUrl : highQualityImgUrl(photo.urlPhotoEdited ? photo.urlPhotoEdited : photo.url),
-						});
-					}
-				});
-			}
+			img.onload = () => resolve(url);
+			img.onerror = () => {
+				attempts++;
+				if (attempts < maxAttempts) {
+					setTimeout(tryLoad, 500);
+				} else {
+					reject(new Error(`No se pudo cargar la imagen: ${url}`));
+				}
+			};
 		};
 
 		imagesInSheet(1);
