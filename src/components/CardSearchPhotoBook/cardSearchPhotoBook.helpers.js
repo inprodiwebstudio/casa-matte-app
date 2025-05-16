fd6340a3ad949474547133417766ff7da473bd4d
JoabMedel <joab_medel99@hotmail.com>
2025-05-15 21:51:38 -0600

    delete attemps

--- src/components/CardSearchPhotoBook/cardSearchPhotoBook.helpers.js
+++ src/components/CardSearchPhotoBook/cardSearchPhotoBook.helpers.js
@@ -1,21 +1,11 @@
 import highQualityImgUrl from "helpers/Functions/highQualityImg";
-export const loadImageWithRetry = (url, maxAttempts = 1) => {
+export const loadImageWithRetry = (url) => {
 	return new Promise((resolve, reject) => {
-		let attempts = 0;
-
 		const tryLoad = () => {
 			const img = new Image();
 			img.src = highQualityImgUrl(url);
 
 			img.onload = () => resolve(url);
-			img.onerror = () => {
-				attempts++;
-				if (attempts < maxAttempts) {
-					setTimeout(tryLoad, 500);
-				} else {
-					reject(new Error(`No se pudo cargar la imagen: ${url}`));
-				}
-			};
 		};
 
 		tryLoad();
