b5a9169a9a242822909c7286421f047f46ea56a4
JoabMedel <joab_medel99@hotmail.com>
2025-05-15 23:16:01 -0600

    add new async map

--- src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
+++ src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
@@ -121,7 +121,7 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 	const validateAllImages = async (imageUrls) => {
 		console.log(imageUrls.length);
 		try {
-		  await Promise.allSettled(imageUrls.map((url) => loadImageWithRetry(url)));
+		  await Promise.allSettled(imageUrls.map(async (url) => await loadImageWithRetry(url)));
 		  return true;
 		} catch (error) {
 		  console.error("Error cargando imágenes:", error.message);
