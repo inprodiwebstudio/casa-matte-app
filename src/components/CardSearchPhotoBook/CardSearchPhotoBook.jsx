87c6dfc8cf56f7b417e92850e959c9db85ddcbd2
JoabMedel <joab_medel99@hotmail.com>
2025-06-02 15:53:01 -0600

    fix map filtered photoBook

--- src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
+++ src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
@@ -33,7 +33,8 @@ const CardSearchPhotoBook = () => {
 
 		try {
 			const photoBookData = await fetchPhotoBook({ module : `wp-json/wp/v2/photobook-2-0/meta/?meta_value=${myOreder}`}).unwrap();
-			const constructorPhotoBooksData = photoBookData.map((photoBook) => ({
+			const filteredBooks = photoBookData.filter((photoBook) => (photoBook?.metas?.status[0] === "48") || (photoBook?.metas?.status[0] === "26"));
+			const constructorPhotoBooksData = filteredBooks.map((photoBook) => ({
 				id   : photoBook?.id,
 				meta : {
 					config           : photoBook?.metas?.config[0],
