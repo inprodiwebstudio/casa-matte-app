3ceb6fb70e7d649b334cb25d8f1f157f8edb447c
JoabMedel <joab_medel99@hotmail.com>
2025-06-10 10:37:25 -0600

    access user photoBook when done

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -425,7 +425,7 @@ const CorrectAccessGuard = () => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
 			addCurrentPhotoBookConfig(photobookData);
-			setStatusView("done");
+			setStatusView("continue");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -450,7 +450,7 @@ const CorrectAccessGuard = () => {
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
 				addCurrentPhotoBookConfig(photobookData);
-				setStatusView("done");
+				setStatusView("continue");
 				return;
 			}
 			handlerAvailableExtra(photobookData?.meta?.id_pedido_hojas_extra);
