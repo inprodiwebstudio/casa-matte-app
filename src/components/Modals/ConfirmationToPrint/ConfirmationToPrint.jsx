e159c22010b843cbaa6395c6dac67817b008db05
Joab Medel <65578244+JoabMedel@users.noreply.github.com>
2025-06-10 13:11:56 -0600

    Development (#51)
    * create a new branch
    
    * List of orders
    
    * change size table height
    
    * Fix spinner charge in table admin
    
    * change envs paths
    
    * View in progress photoBook admin list
    
    * Change regex to parse photos change to jpg
    
    * add product layflat Complte and fix
    
    * fix helper heic to png
    
    * change heic to png list of gallery
    
    * fix BookPages isInDouble page handler
    
    * change text complete photoBook page
    
    * revert changes to not transform base64
    
    * fix helpers base cardSearchPhotoBook
    
    * fix photoBookDownload
    
    * delete high resolution images precharge
    
    * revert with high resolution
    
    * change max attemps load images
    
    * delete attemps
    
    * new changes resolved
    
    * delete high resolution images validate
    
    * delete temporal incompleted pages
    
    * change validate and precharge images
    
    * add correct count number of text containers verticalMedium format
    
    * fix quantity text container in largeVertical Format
    
    * New config to detect number of text container
    
    * resolved pdf layflat squareLarge
    
    * download to images pdf layflat
    
    * Chnage name zip and folder download jpg Pdf
    
    * delete logs and create state to consume text
    
    * change key product luna de miel vertical large format
    
    * hange size models image and text fix hidden text horizontal medium
    
    * fix filter photoBooks
    
    * fix map filtered photoBook
    
    * handler change state of text
    
    * refactor and fix consume state of text in progress
    
    * fix horizontal medium layflat
    
    * fix layflat horizontal mods layouts
    
    * revert changes to original EditTextComponent
    
    * revert changes development Worspace component
    
    * fix styles horizontal medium in workspaces
    
    * fix pdf horizontal layFlat
    
    * fix pdf layflat horizontal
    
    * resolved horizontal medium layflat
    
    * handler leave photos when deleted
    
    * add handler incompleted photoBook

--- src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
+++ src/components/Modals/ConfirmationToPrint/ConfirmationToPrint.jsx
@@ -8,36 +8,39 @@ import "./ConfirmationPrint.scss";
 import axios                    from "axios";
 import { PostingConfig }        from "Notifications";
 import { closeAllModals }       from "@mantine/modals";
+import { isValidArray }         from "helpers";
+import { inCompletePages }      from "./ConfirmationToPrint.helpers";
+import IncompletedPagesBody     from "./IncompletedPagesBody";
 
 
 const ConfirmationToPrint = ({ innerProps }) => {
 	const { postId } = innerProps;
 	const { handlerExtraCost } = useExtraPriceHandler();
 
-	// const pages = useSelector((state) => state.workSpaceSlice.data?.pages, shallowEqual);
+	const pages = useSelector((state) => state.workSpaceSlice.data?.pages, shallowEqual);
 	const userId = useSelector((state) => state.authSlice?.user?.userId, shallowEqual);
 	const userEmail = useSelector((state) => state.authSlice?.user?.email, shallowEqual);
 
 	const [ dataMutation, { isLoading } ] = genericApi.useSubmitDataMutation();
 
-	// const [notCompletedPages, setNotCompletedPages] = useState([]);
+	const [notCompletedPages, setNotCompletedPages] = useState([]);
 
 	const [isLoadingOrder, setIsLoadingOrder] = useState(false);
 
 	const handlerSubmit = async () => {
-		// if (
-		// 	isValidArray(
-		// 		inCompletePages(Object.values(pages))
-		// 	)
-		// ) {
-		// 	setNotCompletedPages(inCompletePages(Object.values(pages)));
-		// 	return;
-		// }
+		if (
+			isValidArray(
+				inCompletePages(Object.values(pages))
+			)
+		) {
+			setNotCompletedPages(inCompletePages(Object.values(pages)));
+			return;
+		}
 		if (handlerExtraCost() > 0) {
 			setIsLoadingOrder(true);
 			try {
 				const responseCreateOrder = await axios.post(
-					"https://casamatte.com/wp-json/wc/v3/orders",
+					"https://casamatte.wip-inprodi.com/wp-json/wc/v3/orders",
 					{
 						payment_method       : "bacs",
 						payment_method_title : "Direct Bank Transfer",
@@ -113,10 +116,13 @@ const ConfirmationToPrint = ({ innerProps }) => {
 
 	return (
 		<>
-			<BodyConfirm
-				onSubmit={handlerSubmit}
-				isLoading={isLoading || isLoadingOrder}
-			/>
+			{
+				!isValidArray(notCompletedPages) ?
+					<BodyConfirm
+						onSubmit={handlerSubmit}
+						isLoading={isLoading || isLoadingOrder}
+					/> : <IncompletedPagesBody pages={notCompletedPages} />
+			}
 		</>
 	);
 };
