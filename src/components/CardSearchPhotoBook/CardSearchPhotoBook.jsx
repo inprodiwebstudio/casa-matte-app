321f93b2814e9bdcbf3667e0f5d233218e50341b
JoabMedel <joab_medel99@hotmail.com>
2025-06-11 10:47:25 -0600

    add log inspect map list orders

--- src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
+++ src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
@@ -45,6 +45,7 @@ const CardSearchPhotoBook = () => {
 					status           : photoBook?.metas?.status[0],
 				},
 			}));
+			console.log(constructorPhotoBooksData);
 			const photoBooksFiltered = constructorPhotoBooksData.filter((photoBook) => (photoBook?.meta?.status === "48") || (photoBook?.meta?.status === "26"));
 			setPhotoBooksOrders(photoBooksFiltered);
 		} catch (error) {
