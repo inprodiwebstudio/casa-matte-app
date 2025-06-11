6e4608686a9066c227a712fbc5b5d9a049497890
Joab Medel <65578244+JoabMedel@users.noreply.github.com>
2025-06-11 10:03:37 -0600

    Development (#53)
    * create a new branch
    
    * List of orders
    
    * change size table height
    
    * Fix spinner charge in table admin
    
    * change envs paths
    
    * View in progress photoBook admin list
    
    * Change regex to parse photos change to jpg
    
    * add product layflat Complte and fix
    
    * fix helper heic to png
    
    * change heic to png list of gallery
    
    * fix BookPages isInDouble page handler
    
    * change text complete photoBook page
    
    * revert changes to not transform base64
    
    * fix helpers base cardSearchPhotoBook
    
    * fix photoBookDownload
    
    * delete high resolution images precharge
    
    * revert with high resolution
    
    * change max attemps load images
    
    * delete attemps
    
    * new changes resolved
    
    * delete high resolution images validate
    
    * delete temporal incompleted pages
    
    * change validate and precharge images
    
    * add correct count number of text containers verticalMedium format
    
    * fix quantity text container in largeVertical Format
    
    * New config to detect number of text container
    
    * resolved pdf layflat squareLarge
    
    * download to images pdf layflat
    
    * Chnage name zip and folder download jpg Pdf
    
    * delete logs and create state to consume text
    
    * change key product luna de miel vertical large format
    
    * hange size models image and text fix hidden text horizontal medium
    
    * fix filter photoBooks
    
    * fix map filtered photoBook
    
    * handler change state of text
    
    * refactor and fix consume state of text in progress
    
    * fix horizontal medium layflat
    
    * fix layflat horizontal mods layouts
    
    * revert changes to original EditTextComponent
    
    * revert changes development Worspace component
    
    * fix styles horizontal medium in workspaces
    
    * fix pdf horizontal layFlat
    
    * fix pdf layflat horizontal
    
    * resolved horizontal medium layflat
    
    * handler leave photos when deleted
    
    * add handler incompleted photoBook
    
    * add new page layflat
    
    * test logs search orders

--- src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
+++ src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
@@ -34,6 +34,7 @@ const CardSearchPhotoBook = () => {
 		try {
 			const photoBookData = await fetchPhotoBook({ module : `wp-json/wp/v2/photobook-2-0/meta/?meta_value=${myOreder}`}).unwrap();
 			const filteredBooks = photoBookData.filter((photoBook) => (photoBook?.metas?.status[0] === "48") || (photoBook?.metas?.status[0] === "26"));
+			console.log(filteredBooks);
 			const constructorPhotoBooksData = filteredBooks.map((photoBook) => ({
 				id   : photoBook?.id,
 				meta : {
