e4382bccd401e1e00146992cc6e62e87bebf6644
JoabMedel <joab_medel99@hotmail.com>
2025-06-11 10:38:44 -0600

    resolved bug notFound orders

--- src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
+++ src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
@@ -37,7 +37,7 @@ const CardSearchPhotoBook = () => {
 			const constructorPhotoBooksData = filteredBooks.map((photoBook) => ({
 				id   : photoBook?.id,
 				meta : {
-					config           : photoBook?.metas?.config[0],
+					config           : photoBook?.metas?.config[0] ?? "",
 					modelo           : photoBook?.metas?.modelo[0],
 					tamano           : photoBook?.metas?.tamano[0],
 					id_del_pedido    : photoBook?.metas?.id_del_pedido[0],
@@ -45,7 +45,6 @@ const CardSearchPhotoBook = () => {
 					status           : photoBook?.metas?.status[0],
 				},
 			}));
-			console.log(constructorPhotoBooksData);
 			const photoBooksFiltered = constructorPhotoBooksData.filter((photoBook) => (photoBook?.meta?.status === "48") || (photoBook?.meta?.status === "26"));
 			setPhotoBooksOrders(photoBooksFiltered);
 		} catch (error) {
