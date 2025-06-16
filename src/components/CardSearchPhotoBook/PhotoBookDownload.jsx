93d6113454c5b9828c0ccc0981dd6c40fbd3f811
JoabMedel <joab_medel99@hotmail.com>
2025-06-16 09:16:14 -0600

    optimized download

--- src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
+++ src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
@@ -1,8 +1,8 @@
-import { Card, Stack, Divider, Text, Group, Button, Progress }        from "@mantine/core";
-import { convertToArray, convertToObject, isValidArray, textToImage } from "helpers";
-import { useEffect, useState }                                        from "react";
-import { SaveIcom }                                                   from "Resources/icons";
-import { handlerIdsTextPages, listTextPagesAvailable }                from "./cardSearchPhotoBook.helpers";
+import { Card, Stack, Divider, Text, Group, Button, Progress }             from "@mantine/core";
+import { convertToArray, convertToObject, isValidArray, textToImage }      from "helpers";
+import { useEffect, useState }                                             from "react";
+import { SaveIcom }                                                        from "Resources/icons";
+import { handlerIdsTextPages, listTextPagesAvailable, loadImageWithRetry } from "./cardSearchPhotoBook.helpers";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { PDFDocument }    from "pdf-lib";
 import SpinePhotoBook     from "components/MyModsLayouts/SpinePdf";
@@ -111,34 +111,34 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 		setPhotoBookConfigData({ ...parseJSON });
 	};
 
-	// const listOfPhotos = (pages) => {
-	// 	const urlPhotos = [];
-	// 	const listOfPages = convertToArray(pages);
+	const listOfPhotos = (pages) => {
+		const urlPhotos = [];
+		const listOfPages = convertToArray(pages);
 
-	// 	listOfPages.forEach((page) => {
-	// 		const photosSheet1 = convertToArray(page?.sheet1?.photos);
-	// 		const photosSheet2 = page?.sheet2?.photos ? convertToArray(page?.sheet2?.photos) : undefined;
+		listOfPages.forEach((page) => {
+			const photosSheet1 = convertToArray(page?.sheet1?.photos);
+			const photosSheet2 = page?.sheet2?.photos ? convertToArray(page?.sheet2?.photos) : undefined;
 
-	// 		const listPhotosSheet1 = photosSheet1.filter(photo => ((photo?.url !== "") && (photo?.id !== "")));
-	// 		const listPhotosSheet2 = photosSheet2 ? photosSheet2.filter(photo => ((photo?.url !== "") && (photo?.id !== ""))) : [];
+			const listPhotosSheet1 = photosSheet1.filter(photo => ((photo?.url !== "") && (photo?.id !== "")));
+			const listPhotosSheet2 = photosSheet2 ? photosSheet2.filter(photo => ((photo?.url !== "") && (photo?.id !== ""))) : [];
 
-	// 		const allPhotos = [...listPhotosSheet1, ...listPhotosSheet2];
-	// 		const allUrl = allPhotos.map(photo => {
-	// 			if (photo?.urlPhotoEdited) {
-	// 				return photo?.urlPhotoEdited;
-	// 			}
-	// 			return photo?.url;
-	// 		});
+			const allPhotos = [...listPhotosSheet1, ...listPhotosSheet2];
+			const allUrl = allPhotos.map(photo => {
+				if (photo?.urlPhotoEdited) {
+					return photo?.urlPhotoEdited;
+				}
+				return photo?.url;
+			});
 
-	// 		urlPhotos.push(...allUrl);
-	// 	});
-	// 	return urlPhotos;
-	// };
+			urlPhotos.push(...allUrl);
+		});
+		return urlPhotos;
+	};
 
-	// const validateAllImages = async (imageUrls) => {
-	// 	const results = await Promise.allSettled(imageUrls.map((url) => loadImageWithRetry(url)));
-	// 	return results.every(result => result.status === "fulfilled");
-	// };
+	const validateAllImages = async (imageUrls) => {
+		const listImgsUrls = imageUrls.map(url => loadImageWithRetry(url));
+		return Promise.all(listImgsUrls).then(() => true).catch(() => false);
+	};
 
 	const handlerFormat = (productType, format) => {
 		if (productType === "travelcoffeetable ") {
@@ -264,7 +264,7 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 
 			for (let i = 0; i < chunks.length; i++) {
 				setProgress(Math.round((i / chunks.length) * 90));
-				await new Promise(resolve => setTimeout(resolve, 300));
+				await new Promise(resolve => setTimeout(resolve, 200));
 
 				const chunkBlob = await pdf(
 					<Document>
@@ -378,16 +378,16 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 		setProgress(0);
 
 		try {
-			// const urlPhotos = listOfPhotos(photoBookConfigData?.pages);
-			// setGenerationStatus("validating_images");
-			// setProgress(10);
+			const urlPhotos = listOfPhotos(photoBookConfigData?.pages);
+			setGenerationStatus("validating_images");
+			setProgress(10);
 
-			// const imagesOk = await validateAllImages(urlPhotos);
-			// if (!imagesOk) {
-			// 	setIsLoading(false);
-			// 	setGenerationStatus("error");
-			// 	return;
-			// }
+			const imagesOk = await validateAllImages(urlPhotos);
+			if (!imagesOk) {
+				setIsLoading(false);
+				setGenerationStatus("error");
+				return;
+			}
 
 			const listPages = convertToArray(photoBookConfigData?.pages);
 
--- src/helpers/Functions/highQualityImg.js
+++ src/helpers/Functions/highQualityImg.js
@@ -2,7 +2,7 @@ const constructorImg = (urlimg) => {
 	if (!urlimg) return;
 	const splitImage = urlimg.split("upload");
 
-	return `${splitImage[0]}upload/q_80/v1/${urlimg.split("/v1/")[1]}`;
+	return `${splitImage[0]}upload/c_fill,w_1600,q_auto,f_auto/v12345/${urlimg.split("/v1/")[1]}`;
 };
 
 export default constructorImg;
