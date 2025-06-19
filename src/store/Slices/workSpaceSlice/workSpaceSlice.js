d40a0d4811999d41cfbbb9f68fd3147927eef342
JoabMedel <joab_medel99@hotmail.com>
2025-06-19 10:06:58 -0600

    fix slice removephoto in layout

--- src/store/Slices/workSpaceSlice/workSpaceSlice.js
+++ src/store/Slices/workSpaceSlice/workSpaceSlice.js
@@ -545,58 +545,59 @@ export const workSpaceSlice = createSlice({
 			let stringyDataPages = JSON.stringify(cloneDataPages);
 
 			imagesIds.forEach(id => {
-				const regex = new RegExp(`"${id}"`, "g");
-				stringyDataPages = stringyDataPages.replace(regex, "\"\"");
+				const regex = new RegExp(id, "g");
+				stringyDataPages = stringyDataPages.replace(regex, "");
 			});
 
 			const dataPagesLeaveImages = JSON.parse(stringyDataPages);
-			const listOfPagesLeaveImages = [...convertToArray(dataPagesLeaveImages)];
+			console.log(dataPagesLeaveImages);
+			// const listOfPagesLeaveImages = [...convertToArray(dataPagesLeaveImages)];
 
-			const parseDeleteImagesSheet = (sheetData) => {
-				const listOfPhotos = convertToArray(sheetData?.photos);
+			// const parseDeleteImagesSheet = (sheetData) => {
+			// 	const listOfPhotos = convertToArray(sheetData?.photos);
 
-				if (isValidArray(listOfPhotos)) {
-					const isNotAvailablePhotos = (listOfPhotos.length === 1) && (!listOfPhotos[0]?.id && !listOfPhotos[0]?.url);
-					if (isNotAvailablePhotos) {
-						return sheetData;
-					}
-					const newListOfPhotos = listOfPhotos.map((photo) => {
-						if ((photo?.id === "") && photo?.url) {
-							return {
-								...photo,
-								url            : "",
-								urlPhotoEdited : "",
-							};
-						}
-						return photo;
-					});
-					const newObjectPhotos = newListOfPhotos.reduce((acc, photo, index) => {
-						acc[index] = photo;
-						return acc;
-					});
-					return {
-						...sheetData,
-						photos : newObjectPhotos,
-					};
-				}
+			// 	if (isValidArray(listOfPhotos)) {
+			// 		const isNotAvailablePhotos = (listOfPhotos.length === 1) && (!listOfPhotos[0]?.id && !listOfPhotos[0]?.url);
+			// 		if (isNotAvailablePhotos) {
+			// 			return sheetData;
+			// 		}
+			// 		const newListOfPhotos = listOfPhotos.map((photo) => {
+			// 			if ((photo?.id === "") && photo?.url) {
+			// 				return {
+			// 					...photo,
+			// 					url            : "",
+			// 					urlPhotoEdited : "",
+			// 				};
+			// 			}
+			// 			return photo;
+			// 		});
+			// 		const newObjectPhotos = newListOfPhotos.reduce((acc, photo, index) => {
+			// 			acc[index] = photo;
+			// 			return acc;
+			// 		});
+			// 		return {
+			// 			...sheetData,
+			// 			photos : newObjectPhotos,
+			// 		};
+			// 	}
 
-				return sheetData;
-			};
+			// 	return sheetData;
+			// };
 
-			const listOfPagesDeletedImages = listOfPagesLeaveImages.map((pageData) => {
-				return {
-					...pageData,
-					sheet1 : parseDeleteImagesSheet(pageData?.sheet1),
-					...(pageData?.sheet2 && {sheet2 : parseDeleteImagesSheet(pageData?.sheet2)}),
-				};
-			});
+			// const listOfPagesDeletedImages = listOfPagesLeaveImages.map((pageData) => {
+			// 	return {
+			// 		...pageData,
+			// 		sheet1 : parseDeleteImagesSheet(pageData?.sheet1),
+			// 		...(pageData?.sheet2 && {sheet2 : parseDeleteImagesSheet(pageData?.sheet2)}),
+			// 	};
+			// });
 
-			const newPagesDataDeletedImages = convertToObject(listOfPagesDeletedImages);
+			// const newPagesDataDeletedImages = convertToObject(listOfPagesDeletedImages);
 
-			state.data = {
-				...state.data,
-				pages : newPagesDataDeletedImages,
-			};
+			// state.data = {
+			// 	...state.data,
+			// 	pages : newPagesDataDeletedImages,
+			// };
 		},
 		autoFillImages : (state, {payload}) => {
 			const newData = {FrontLayout : {...state.data.frontPage}, ...state?.data?.pages};
