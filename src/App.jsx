7fe20a80757537280ae377deb469959c0a6edfb6
JoabMedel <joab_medel99@hotmail.com>
2025-07-03 09:15:35 -0600

    add new editor photos

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
 
 	window.addEventListener("popstate", (event) => {
 		event.preventDefault();
-		window.location.href = "https://casamatte.wip-inprodi.com/dashboard/";
+		window.location.href = "https://casamatte.com/dashboard/";
 	});
 
 	if (isMobile) {
