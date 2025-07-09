7a9b1c60c4d89b4282702dfbf6c685fa1bf21a2d
JoabMedel <joab_medel99@hotmail.com>
2025-07-09 13:22:31 -0600

    increment quality print imgs

--- src/helpers/Functions/highQualityImg.js
+++ src/helpers/Functions/highQualityImg.js
@@ -2,7 +2,7 @@ const constructorImg = (urlimg) => {
 	if (!urlimg) return;
 	const splitImage = urlimg.split("upload");
 
-	return `${splitImage[0]}upload/c_fill,w_3000,q_auto,f_auto/v12345/${urlimg.split("/v1/")[1]}`;
+	return `${splitImage[0]}upload/c_fill,w_3200,q_auto,f_auto/v12345/${urlimg.split("/v1/")[1]}`;
 };
 
 export default constructorImg;
