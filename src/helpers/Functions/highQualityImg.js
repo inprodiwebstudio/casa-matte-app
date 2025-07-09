2ba57bdb94a5c95fcac92b44b07add239c6970da
JoabMedel <joab_medel99@hotmail.com>
2025-07-09 12:39:05 -0600

    add new size high quality

--- src/helpers/Functions/highQualityImg.js
+++ src/helpers/Functions/highQualityImg.js
@@ -2,7 +2,7 @@ const constructorImg = (urlimg) => {
 	if (!urlimg) return;
 	const splitImage = urlimg.split("upload");
 
-	return `${splitImage[0]}upload/c_fill,w_1900,q_auto,f_auto/v12345/${urlimg.split("/v1/")[1]}`;
+	return `${splitImage[0]}upload/c_fill,w_2400,q_auto,f_auto/v12345/${urlimg.split("/v1/")[1]}`;
 };
 
 export default constructorImg;
