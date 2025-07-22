51a1c7e3375a2039c33a6c56fb253f3798f26bf6
JoabMedel <joab_medel99@hotmail.com>
2025-07-22 11:33:26 -0600

    fix layouts Horizontal Medium LayFlat

---
+++ src/components/MyModsLayouts/LayFlatHorizontalMedium/PdfMods/Regular/Mod63Pdf.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+
+//Own components
+import Html           from "react-pdf-html";
+import ReactDOMServer from "react-dom/server";
+import { imgUrlPdf }  from "helpers";
+// eslint-disable-next-line import/extensions
+
+const Mod63Pdf = ({images}) => {
+	const bodyHtml = (
+		<div
+			style={{
+				height        : "100%",
+				width         : "100%",
+				display       : "flex",
+				flexDirection : "row",
+				gap           : "10px",
+				paddingTop    : "10%",
+				paddingBottom : "10%",
+				paddingLeft   : "2%",
+				paddingRight  : "2%",
+			}}
+		>
+			<div
+				style={{
+					height     : "100%",
+					width      : "calc(20% - 2px)",
+					background : "#E3E3E3",
+				}}
+			>
+				{
+					images[0]?.url && (
+						<img
+							src={imgUrlPdf(images[0])}
+							alt={images[0]?.url}
+							style={{
+								objectFit : "cover",
+								height    : "100%",
+							}}
+						/>
+					)
+				}
+			</div>
+			<div
+				style={{
+					height     : "100%",
+					width      : "calc(20% - 2px)",
+					background : "#E3E3E3",
+				}}
+			>
+				{
+					images[1]?.url && (
+						<img
+							src={imgUrlPdf(images[1])}
+							alt={images[1]?.url}
+							style={{
+								objectFit : "cover",
+								height    : "100%",
+							}}
+						/>
+					)
+				}
+			</div>
+			<div
+				style={{
+					height     : "100%",
+					width      : "calc(20% - 2px)",
+					background : "#E3E3E3",
+				}}
+			>
+				{
+					images[2]?.url && (
+						<img
+							src={imgUrlPdf(images[2])}
+							alt={images[2]?.url}
+							style={{
+								objectFit : "cover",
+								height    : "100%",
+							}}
+						/>
+					)
+				}
+			</div>
+			<div
+				style={{
+					height     : "100%",
+					width      : "calc(20% - 2px)",
+					background : "#E3E3E3",
+				}}
+			>
+				{
+					images[3]?.url && (
+						<img
+							src={imgUrlPdf(images[3])}
+							alt={images[3]?.url}
+							style={{
+								objectFit : "cover",
+								height    : "100%",
+							}}
+						/>
+					)
+				}
+			</div>
+			<div
+				style={{
+					height     : "100%",
+					width      : "calc(20% - 2px)",
+					background : "#E3E3E3",
+				}}
+			>
+				{
+					images[4]?.url && (
+						<img
+							src={imgUrlPdf(images[4])}
+							alt={images[4]?.url}
+							style={{
+								objectFit : "cover",
+								height    : "100%",
+							}}
+						/>
+					)
+				}
+			</div>
+		</div>
+	);
+
+	const toPdfElement = ReactDOMServer.renderToStaticMarkup(bodyHtml);
+
+	return (
+		<Html>{toPdfElement}</Html>
+	);
+};
+
+export default Mod63Pdf;
