d268924ccdd60b493469140839f12193a9319566
JoabMedel <joab_medel99@hotmail.com>
2025-07-30 16:50:33 -0600

    resolved conflicts helper textToImage file

--- src/helpers/textToImage.js
+++ src/helpers/textToImage.js
@@ -3,17 +3,15 @@ import domtoimage from "dom-to-image";
 
 const textToImage = async (id) => {
 	const element = document.getElementById(id);
-	const style = window.getComputedStyle(element);
 
-	const width = element.clientWidth;
-	const height =  parseFloat(style.height);
+	if (!element) return undefined;
 
 	const width = element.clientWidth * 5;
 	const height = element.clientHeight * 5;
 
 	const imgData = await domtoimage.toPng(element, {
-		width  : width * 5,
-		height : height * 1.6,
+		width  : width,
+		height : height,
 		style  : {
 			transform       : `scale(${5})`,
 			transformOrigin : "top left",
