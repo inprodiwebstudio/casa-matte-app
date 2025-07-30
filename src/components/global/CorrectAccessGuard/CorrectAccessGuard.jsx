632ab88c2f0d7c10ecc61d055619872f585643dc
JoabMedel <joab_medel99@hotmail.com>
2025-07-30 13:51:40 -0600

    revert changes

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,7 +427,7 @@ const CorrectAccessGuard = () => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
 			addCurrentPhotoBookConfig(photobookData);
-			setStatusView("continue");
+			setStatusView("done");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -451,7 +451,7 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
-				setStatusView("continue");
+				setStatusView("done");
 				return;
 			}
 			handlerAvailableExtra(photobookData?.meta?.id_pedido_hojas_extra);
