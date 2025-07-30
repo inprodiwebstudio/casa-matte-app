84471b102e92a8c4aced3c759d50823ce4d2dfb9
JoabMedel <joab_medel99@hotmail.com>
2025-07-30 16:32:47 -0600

    revert original fn textToImage

--- src/helpers/textToImage.js
+++ src/helpers/textToImage.js
@@ -3,21 +3,20 @@ import domtoimage from "dom-to-image";
 
 const textToImage = async (id) => {
 	const element = document.getElementById(id);
-	const style = window.getComputedStyle(element);
-
-	const width = element.clientWidth;
-	const height =  parseFloat(style.height);
 
 	if (!element) return undefined;
 
+	const width = element.clientWidth * 5; // Aumenta el ancho
+	const height = element.clientHeight * 5; // Aumenta la altura
+
 	const imgData = await domtoimage.toPng(element, {
-		width  : width * 5,
-		height : height * 1.6,
+		width  : width,
+		height : height,
 		style  : {
 			transform       : `scale(${5})`,
 			transformOrigin : "top left",
-			width           : `${width}px`, // Mantiene el tamaño real en el DOM
-			height          : `${height}px`,
+			width           : `${element.clientWidth}px`, // Mantiene el tamaño real en el DOM
+			height          : `${element.clientHeight}px`,
 		},
 	});
 
