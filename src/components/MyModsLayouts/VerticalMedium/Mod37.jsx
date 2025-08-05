cdde8083acd98b203e408f11a1ab52d8d256e7d3
JoabMedel <joab_medel99@hotmail.com>
2025-08-05 07:32:24 -0600

    test mod37 layout vertical medium

--- src/components/MyModsLayouts/VerticalMedium/Mod37.jsx
+++ src/components/MyModsLayouts/VerticalMedium/Mod37.jsx
@@ -37,7 +37,7 @@ const Mod37 = ({
 							alignItems     : "center",
 							overflow       : "visible",
 							maxHeight      : "100px",
-							background     : "red !important",
+							background     : "red",
 						}}
 					>
 						<div
