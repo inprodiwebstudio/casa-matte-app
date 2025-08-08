f2a4f6b1ba97c7506c1a2dac0f99a871ddb1866b
JoabMedel <joab_medel99@hotmail.com>
2025-08-08 06:55:08 -0600

    to fix Mod35Pdf vertical medium

--- src/components/MyModsLayouts/VerticalMedium/Mod35.jsx
+++ src/components/MyModsLayouts/VerticalMedium/Mod35.jsx
@@ -17,33 +17,37 @@ const Mod35 = ({
 	const defaultText01 = "<p style='text-align: center;'><span style='font-size: 42px; font-family: JosefinSans-Light;'>TÍTULO 1</span></p>";
 
 	return (
-		<Center
-			w="100%"
-			h="100%"
+		<div
+			style={{
+				width        : "100%",
+				height       : "100%",
+			}}
 			{...(isInWorkSpace && { id : `${pageNo}-${modLayout}-text1` })}
 		>
-			<Stack
-				w="70%"
-				p="0%"
-				pt="0%"
-				pb="0%"
-			>
-				<Text
-					sizes={{
-						"chico"   : "38px",
-						"regular" : "42px",
-						"grande"  : "46px",
-					}}
-					sheetNo={sheetNo}
-					textShell={() => <TextShell.Title />}
-					letterSpacing="6.5px"
-					data={textInsertion(data?.text[0], defaultText01, isInWorkSpace)}
-					isInPaginator={isInPaginator}
-					isThumbNail={isThumbNail}
-					textNo={0}
-				/>
-			</Stack>
-		</Center>
+			<Center w="100%" h="100%">
+				<Stack
+					w="70%"
+					p="0%"
+					pt="0%"
+					pb="0%"
+				>
+					<Text
+						sizes={{
+							"chico"   : "38px",
+							"regular" : "42px",
+							"grande"  : "46px",
+						}}
+						sheetNo={sheetNo}
+						textShell={() => <TextShell.Title />}
+						letterSpacing="6.5px"
+						data={textInsertion(data?.text[0], defaultText01, isInWorkSpace)}
+						isInPaginator={isInPaginator}
+						isThumbNail={isThumbNail}
+						textNo={0}
+					/>
+				</Stack>
+			</Center>
+		</div>
 	);
 };
 
