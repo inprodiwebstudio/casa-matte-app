2c37580ce9c33ac8bcc9a4954e3335449216b5b6
JoabMedel <joab_medel99@hotmail.com>
2025-08-11 11:12:43 -0600

    change fn convert text to imgs

--- src/helpers/textToImage.js
+++ src/helpers/textToImage.js
@@ -14,7 +14,7 @@ const textToImage = async (id) => {
 		height : height,
 		style  : {
 			transform       : `scale(${5})`,
-			transformOrigin : "top left",
+			transformOrigin : "top",
 			width           : `${element.clientWidth}px`, // Mantiene el tamaño real en el DOM
 			height          : `${element.clientHeight}px`,
 		},
