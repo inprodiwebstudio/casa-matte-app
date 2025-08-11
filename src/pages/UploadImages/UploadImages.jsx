fc5a8950623f1cb0b2dd374e17a9d1dbf67eb53b
JoabMedel <joab_medel99@hotmail.com>
2025-08-11 13:56:52 -0600

    change component DropDoc photos to general use

--- src/pages/UploadImages/UploadImages.jsx
+++ src/pages/UploadImages/UploadImages.jsx
@@ -1,8 +1,21 @@
 import { Center, Stack } from "@mantine/core"
 import { BlankPage } from "core/components"
 import DropDoc from "components/Gallery/DropDoc"
+import { useEffect } from "react";
+import { authSlice } from "store/Slices";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
 
 const UploadImages = () => {
+  const { authorId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authSlice.actions.setUserData({
+        userId   : authorId ?? undefined,
+    }));
+  }, [authorId]);
+
   return (
     <BlankPage
       backgroundColor="#F7F5F1"
