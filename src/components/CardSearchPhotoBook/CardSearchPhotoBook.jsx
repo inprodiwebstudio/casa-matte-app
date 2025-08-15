5387c3547f2f37797486225088db178afe10c40e
JoabMedel <joab_medel99@hotmail.com>
2025-08-15 10:45:30 -0600

    to revert and fix list post software and search by order

--- src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
+++ src/components/CardSearchPhotoBook/CardSearchPhotoBook.jsx
@@ -40,9 +40,11 @@ const CardSearchPhotoBook = () => {
 				id_del_pedido 	 : photoBook?.metas?.id_del_pedido[0],
 				correo_del_autor : photoBook?.metas?.correo_del_autor[0],
 				status           : photoBook?.metas?.status[0],
+				modelo : photoBook?.metas?.modelo[0],
+				tamano : photoBook?.metas?.tamano[0],
 				fecha_de_termino : photoBook?.metas?.fecha_de_termino[0],
 			}));
-			const photoBooksFiltered = constructorPhotoBooksData.filter((photoBook) => (photoBook?.meta?.status === "48") || (photoBook?.meta?.status === "26"));
+			const photoBooksFiltered = constructorPhotoBooksData.filter((photoBook) => (photoBook?.status === "48") || (photoBook?.status === "26"));
 			setPhotoBooksOrders(photoBooksFiltered);
 		} catch (error) {
 			console.error(error);
