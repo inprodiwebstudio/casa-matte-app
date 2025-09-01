aa425aef8da2e83d0142f5a65e9aba87791bf7c1
JoabMedel <joab_medel99@hotmail.com>
2025-09-01 11:55:40 -0600

    add new notification admin handler access

--- src/pages/dashboard/LoginCard/LoginCard.jsx
+++ src/pages/dashboard/LoginCard/LoginCard.jsx
@@ -57,6 +57,25 @@ const LoginCard = () => {
 			if (!isValidEmail || (currentPassword !== passwordAdmin)) {
 				setError("username");
 				setError("password");
+				if (currentEmail !== adminEmail) {
+					showNotification({
+						title   : "Error al iniciar sesión",
+						message : "El correo es incorrecto",
+						color   : "red",
+						styles  : () => ({
+							root : {
+										  "&::before" : {
+											  borderRadius : "0px",
+											  width        : "3px",
+										  },
+										  borderRadius : "0px",
+							},
+
+							title       : { fontFamily : "Helvetica", fontWeight : "500", textTransform : "uppercase" },
+							description : { fontFamily : "Helvetica" },
+						}),
+					});
+				}
 				if (currentPassword !== passwordAdmin) {
 					showNotification({
 						title   : "Error al iniciar sesión",
