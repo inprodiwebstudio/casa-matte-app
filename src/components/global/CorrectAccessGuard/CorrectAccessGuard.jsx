82368683f367acd2084028ecc90ce9731c63364c
JoabMedel <joab_medel99@hotmail.com>
2025-10-08 09:59:42 -0600

    reverte changes not access book

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,8 +427,7 @@ const CorrectAccessGuard = () => {
 	const handlerAvailableExtra = async (idOrderExtra) => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
-			addCurrentPhotoBookConfig(photobookData);
-			setStatusView("continue");
+			setStatusView("done");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -452,8 +451,7 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
-				addCurrentPhotoBookConfig(photobookData);
-				setStatusView("continue");
+				setStatusView("done");
 				return;
 			}
 			handlerAvailableExtra(photobookData?.meta?.id_pedido_hojas_extra);
