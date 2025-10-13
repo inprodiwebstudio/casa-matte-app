21a81b425ae3fbd4cba7b7cafcf2595c85d979e1
JoabMedel <joab_medel99@hotmail.com>
2025-10-13 05:12:25 -0600

    fix photoGallery component

--- src/components/GalleryEditor/PhotoGallery/Body/GridPhotos/GridPhotos.jsx
+++ src/components/GalleryEditor/PhotoGallery/Body/GridPhotos/GridPhotos.jsx
@@ -1,68 +1,68 @@
-import { Grid, ScrollArea } from "@mantine/core";
-import PhotoCard            from "../PhotoCard";
-import NotAvailablePhotos   from "./NotAvailablePhotos";
-import { isValidArray }     from "helpers";
-// import { useEffect, useState }          from "react";
-// import { shallowEqual, useSelector }    from "react-redux";
+import { Grid, ScrollArea }             from "@mantine/core";
+import PhotoCard                        from "../PhotoCard";
+import NotAvailablePhotos               from "./NotAvailablePhotos";
+import { convertToArray, isValidArray } from "helpers";
+import { useEffect, useState }          from "react";
+import { shallowEqual, useSelector }    from "react-redux";
 
 const GridPhotos = ({
 	cols = 6,
 	photos,
 }) => {
-	// const [ selectedImagesIds, setSelectedImagesIds ] = useState([]);
+	const [ selectedImagesIds, setSelectedImagesIds ] = useState([]);
 
-	// const workSpaceData = useSelector((state) => state.workSpaceSlice.data, shallowEqual);
+	const workSpaceData = useSelector((state) => state.workSpaceSlice.data, shallowEqual);
 
 	const listOfPhotos = photos ?? [];
+
+	const isInUsePhoto = (imageId) => {
+		const findImage = selectedImagesIds.find(id => id === imageId);
+		if (findImage) {
+			return true;
+		}
+		return false;
+	};
+
+	useEffect(() => {
+		if (!workSpaceData) return;
+
+		const pagesList = convertToArray(workSpaceData?.pages);
+		const listOfAllPages = [workSpaceData?.frontPage, ...pagesList];
+		const newDataSelected = [];
+		if (isValidArray(pagesList)) {
+			listOfAllPages.forEach((data, i) => {
+				const isAVailableSheet2 = data?.sheet2;
+				const sheet1Photos = convertToArray(data?.sheet1?.photos);
+
+				if (isValidArray(sheet1Photos)) {
+					sheet1Photos.forEach((photo) => {
+						if (photo?.id !== "") {
+							newDataSelected.push(photo?.id);
+						}
+					});
+				}
+
+				if (isAVailableSheet2) {
+					const sheet2Photos = convertToArray(data?.sheet2?.photos);
+					if (isValidArray(sheet2Photos)) {
+						sheet2Photos.forEach((photo, e) => {
+							if (photo?.id !== "") {
+								newDataSelected.push(photo?.id);
+							}
+						});
+					}
+				}
+			});
+		}
+
+		setSelectedImagesIds(newDataSelected);
+	}, [workSpaceData]);
 
 	if (!isValidArray(listOfPhotos)) {
 		return (
 			<NotAvailablePhotos />
 		);
 	}
-
-	// const isInUsePhoto = (imageId) => {
-	// 	const findImage = selectedImagesIds.find(id => id === imageId);
-	// 	if (findImage) {
-	// 		return true;
-	// 	}
-	// 	return false;
-	// };
-
-	// useEffect(() => {
-	// 	if (!workSpaceData) return;
-
-	// 	const pagesList = convertToArray(workSpaceData?.pages);
-	// 	const listOfAllPages = [workSpaceData?.frontPage, ...pagesList];
-	// 	const newDataSelected = [];
-	// 	if (isValidArray(pagesList)) {
-	// 		listOfAllPages.forEach((data, i) => {
-	// 			const isAVailableSheet2 = data?.sheet2;
-	// 			const sheet1Photos = convertToArray(data?.sheet1?.photos);
-
-	// 			if (isValidArray(sheet1Photos)) {
-	// 				sheet1Photos.forEach((photo) => {
-	// 					if (photo?.id !== "") {
-	// 						newDataSelected.push(photo?.id);
-	// 					}
-	// 				});
-	// 			}
-
-	// 			if (isAVailableSheet2) {
-	// 				const sheet2Photos = convertToArray(data?.sheet2?.photos);
-	// 				if (isValidArray(sheet2Photos)) {
-	// 					sheet2Photos.forEach((photo, e) => {
-	// 						if (photo?.id !== "") {
-	// 							newDataSelected.push(photo?.id);
-	// 						}
-	// 					});
-	// 				}
-	// 			}
-	// 		});
-	// 	}
-
-	// 	setSelectedImagesIds(newDataSelected);
-	// }, [workSpaceData]);
 
 	return (
 		<ScrollArea
@@ -86,7 +86,7 @@ const GridPhotos = ({
 					>
 						<PhotoCard
 							urlImage={item?.url}
-							isInUsePhoto={false}
+							isInUsePhoto={isInUsePhoto(item?.id)}
 							id={item?.id}
 							publicId={item?.public_id}
 							pixels={item?.pixels}
