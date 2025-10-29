821fa881ea697272602312d8482b452217ed2f13
JoabMedel <joab_medel99@hotmail.com>
2025-10-29 11:26:04 -0600

    add configContext file

---
+++ src/contexts/configContext.jsx
@@ -0,0 +1,144 @@
+import { isValidArray }                     from "helpers";
+import {createContext, useState, useEffect} from "react";
+import { useSelector, shallowEqual }        from "react-redux";
+
+//Hooks
+import { useHandlerConfigBook } from "helpers/Hooks/useHandlerConfigBook";
+
+export const currentConfigPhotoBookContext = createContext();
+
+export const CurrentConfigPhotoBookProvider = ({children}) => {
+	const photoCurrentPageData = useSelector((state) => state.workSpaceSlice.currentPageData, shallowEqual);
+
+
+	const {layoutMods} = useHandlerConfigBook() ?? {};
+
+	const defaultDataConfig = {
+		modlayoutId     : undefined,
+		texts           : undefined,
+		linesDecoration : undefined,
+		photos          : undefined,
+	};
+	const [currentConfigPhotoBook, setCurrentConfigPhotoBook] = useState({
+		pageId : undefined,
+		sheet1 : defaultDataConfig,
+		sheet2 : defaultDataConfig,
+	});
+
+	useEffect(() => {
+		if (photoCurrentPageData && layoutMods) {
+			const parseTextsObject = (textObject, modLayout) => {
+				if (!textObject || !modLayout) {
+					return undefined;
+				}
+
+				const listOfTexts = Object.values(textObject);
+
+				if (!isValidArray(listOfTexts)) {
+					return undefined;
+				}
+
+				const isAvailableNewKeys = textObject[0]?.position;
+
+				if (isAvailableNewKeys) {
+					return textObject;
+				}
+
+				const layoutModConfig = layoutMods[modLayout];
+
+				const defaultTexts = layoutModConfig?.defaultTexts;
+
+				const newListOfTexts = listOfTexts.map((item, index) => {
+					const textPresetConfig = defaultTexts?.[index];
+					const {
+						position,
+						sizes,
+						text,
+						letterSpacing,
+						gapSpacing,
+						lineHeight,
+					} = textPresetConfig;
+					return {
+						text,
+						position,
+						sizes,
+						letterSpacing,
+						gapSpacing,
+						lineHeight,
+					};
+				});
+
+				const newTextObject = newListOfTexts.reduce((acc, item, index) => {
+					acc[index] = item;
+					return acc;
+				}, {});
+
+				return newTextObject;
+
+				//The sizes and position format is position : {x:0, y:0} and sizes : {width:0, height:0}
+			};
+
+			const parseLinesDecorationObject = (linesObject, modLayout) => {
+				if (!linesObject || !modLayout) {
+					return undefined;
+				}
+
+				const listOfLines = Object.values(linesObject);
+
+				if (!isValidArray(listOfLines)) {
+					return undefined;
+				}
+
+				const isAvailablePosition = linesObject[0]?.position;
+
+				if (isAvailablePosition) {
+					return linesObject;
+				}
+
+				const layoutModConfig = layoutMods[modLayout];
+
+				const presetLinesDecoration = layoutModConfig?.linesDecoration;
+
+				if (!presetLinesDecoration) {
+					return undefined;
+				}
+
+				const newLinesObjInsert = presetLinesDecoration.reduce((acc, item, index) => {
+					acc[index] = item;
+					return acc;
+				}, {});
+
+				return newLinesObjInsert;
+			};
+
+			setCurrentConfigPhotoBook({
+				pageId : photoCurrentPageData?.id ?? undefined,
+				sheet1 : {
+					modlayoutId     : photoCurrentPageData?.sheet1?.layoutType ?? undefined,
+					texts           : parseTextsObject(photoCurrentPageData?.sheet1?.text ?? undefined, photoCurrentPageData?.sheet1?.layoutType ?? undefined),
+					photos          : photoCurrentPageData?.sheet1?.photos ?? undefined,
+					linesDecoration : parseLinesDecorationObject(photoCurrentPageData?.sheet1?.linesDecoration ?? undefined, photoCurrentPageData?.sheet1?.linesDecoration ?? undefined),
+				},
+				...(photoCurrentPageData?.sheet2 && {
+					sheet2 : {
+						modlayoutId     : photoCurrentPageData?.sheet2?.layoutType ?? undefined,
+						texts           : parseTextsObject(photoCurrentPageData?.sheet2?.text ?? undefined, photoCurrentPageData?.sheet2?.layoutType ?? undefined),
+						photos          : photoCurrentPageData?.sheet2?.photos ?? undefined,
+						linesDecoration : parseLinesDecorationObject(photoCurrentPageData?.sheet2?.linesDecoration ?? undefined, photoCurrentPageData?.sheet2?.linesDecoration ?? undefined),
+					},
+				}),
+			});
+		}
+	}, [photoCurrentPageData, layoutMods]);
+
+	return (
+		<currentConfigPhotoBookContext.Provider
+			value={{
+				currentConfigPhotoBook,
+				setCurrentConfigPhotoBook,
+			}}
+		>
+			{children}
+		</currentConfigPhotoBookContext.Provider>
+	);
+};
