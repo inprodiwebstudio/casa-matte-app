085580124469bb99aaeaf39c76de50da51798287
JoabMedel <joab_medel99@hotmail.com>
2025-10-29 12:27:27 -0600

    add LineDecorationDrag componen and ModTextLayout

---
+++ src/components/global/LineDecorationDrag/LineDecorationDrag.jsx
@@ -0,0 +1,68 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Rnd }                           from "react-rnd";
+import DividerLayout                     from "components/LayoutHandler/DividerLayout";
+import { useContext }                    from "react";
+import { currentConfigPhotoBookContext } from "contexts/configContext";
+
+
+const LineDecorationDrag = ({
+	orientation = "h",
+	long = "100%",
+	weight = "2px",
+	layoutNo,
+	sheetNo,
+}) => {
+	const {currentConfigPhotoBook, setCurrentConfigPhotoBook} = useContext(currentConfigPhotoBookContext);
+
+	const currentPositionLine = currentConfigPhotoBook?.[`sheet${sheetNo}`]?.linesDecoration?.[layoutNo]?.position ?? {
+		x : 0,
+		y : 0,
+	};
+	const handlerSetPosition = (d) => {
+		setCurrentConfigPhotoBook(prev => ({
+			...prev,
+			[`sheet${sheetNo}`] : {
+				...prev?.[`sheet${sheetNo}`],
+				linesDecoration : {
+					...prev?.[`sheet${sheetNo}`]?.linesDecoration,
+					[layoutNo] : {
+						...prev?.[`sheet${sheetNo}`]?.linesDecoration?.[layoutNo],
+						position : {
+							x : d.x,
+							y : d.y,
+						},
+					},
+				},
+			},
+		}));
+	};
+
+	return (
+		<Rnd
+			enableResizing={false}
+			position={currentPositionLine}
+			bounds={`#draggable-zone-sheet${sheetNo}`}
+			size={{
+				width  : long,
+				height : "40px",
+			}}
+			style={{
+				display        : "flex",
+				justifyContent : "center",
+				alignItems     : "center",
+			}}
+			scale={0.55}
+			onDragStop={(e, d) => {
+				handlerSetPosition(d);
+			}}
+		>
+			<DividerLayout
+				long={long}
+				position={orientation}
+				weight={weight}
+			/>
+		</Rnd>
+	);
+};
+
+export default LineDecorationDrag;
---
+++ src/components/global/ModTextLayout/ModTextLayout.jsx
@@ -0,0 +1,54 @@
+import {Stack } from "@mantine/core";
+//Own components
+import Text                              from "components/LayoutHandler/Text";
+import LineDecorationDrag                from "../LineDecorationDrag";
+import { currentConfigPhotoBookContext } from "contexts/configContext";
+import { useContext }                    from "react";
+
+const ModTextLayout = ({
+	sheetNo,
+}) => {
+
+	const {currentConfigPhotoBook} = useContext(currentConfigPhotoBookContext);
+
+	const listOfTexts = Object.values(currentConfigPhotoBook?.[`sheet${sheetNo}`]?.texts ?? {});
+
+	const listOfDecorationLines = Object.values(currentConfigPhotoBook?.[`sheet${sheetNo}`]?.linesDecoration ?? {});
+
+	return (
+		<Stack
+			w="100%"
+			h="100%"
+			p="0%"
+		>
+			{
+				listOfDecorationLines.map((item, index) => {
+					return (
+						<LineDecorationDrag
+							key={index}
+							orientation={item?.orientation}
+							long={item?.long}
+							weight={item?.weight}
+							layoutNo={index}
+							sheetNo={sheetNo}
+						/>
+					);
+				})
+			}
+			{listOfTexts.map((item, index) => {
+				return (
+					<Text
+						key={index}
+						sheetNo={sheetNo}
+						letterSpacing={item?.letterSpacing}
+						gapSpacing={item?.gapSpacing}
+						lineHeight={item?.lineHeight}
+						layoutNo={index}
+					/>
+				);
+			})}
+		</Stack>
+	);
+};
+
+export default ModTextLayout;
