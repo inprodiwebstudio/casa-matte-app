9506aeae812bc953c997e67379cca635b1d7d523
JoabMedel <joab_medel99@hotmail.com>
2025-11-10 11:14:28 -0600

    Fix Mod67 horzintal large

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -124,7 +124,7 @@ const Header = () => {
 	return (
 		<div className="Header">
 			<div className={`body-container ${isPreviewActive && "isActivePreview"}`}>
-				<a href="https://casamatte.wip-inprodi.com/">
+				<a href="https://casamatte.com/">
 					<img src={LogoCasaMatte} width={120} />
 				</a>
 				{
