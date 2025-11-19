1a0a56ae108837d88ae32922d9d175cae7b79775
JoabMedel <joab_medel99@hotmail.com>
2025-11-19 16:03:55 -0600

    Revert access end book

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,17 +427,7 @@ const CorrectAccessGuard = () => {
 	const handlerAvailableExtra = async (idOrderExtra) => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
-			if (postId === "41959") {
-				addCurrentPhotoBookConfig(photobookData);
-				setStatusView("continue");
-				return;
-			}
 			setStatusView("done");
-			return;
-		}
-		if (postId === "41959") {
-			addCurrentPhotoBookConfig(photobookData);
-			setStatusView("continue");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -461,11 +451,6 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
-				if (postId === "41959") {
-					addCurrentPhotoBookConfig(photobookData);
-					setStatusView("continue");
-					return;
-				}
 				setStatusView("done");
 				return;
 			}
