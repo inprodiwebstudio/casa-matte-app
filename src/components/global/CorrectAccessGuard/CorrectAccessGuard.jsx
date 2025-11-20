27ec989a1956e44d22d7947b8272faeb10baf8a9
JoabMedel <joab_medel99@hotmail.com>
2025-11-20 11:08:08 -0600

    Acces book end new customer

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,7 +427,17 @@ const CorrectAccessGuard = () => {
 	const handlerAvailableExtra = async (idOrderExtra) => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
+			if (postId === "38111") {
+				addCurrentPhotoBookConfig(photobookData);
+				setStatusView("continue");
+				return;
+			}
 			setStatusView("done");
+			return;
+		}
+		if (postId === "38111") {
+			addCurrentPhotoBookConfig(photobookData);
+			setStatusView("continue");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -451,6 +461,11 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
+				if (postId === "38111") {
+					addCurrentPhotoBookConfig(photobookData);
+					setStatusView("continue");
+					return;
+				}
 				setStatusView("done");
 				return;
 			}
