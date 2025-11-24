df961be262310b19222e8ef2c08b0c0889ce8771
JoabMedel <joab_medel99@hotmail.com>
2025-11-24 13:48:26 -0600

    Add other access end book user

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,7 +427,7 @@ const CorrectAccessGuard = () => {
 	const handlerAvailableExtra = async (idOrderExtra) => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
-			if (postId === "43112") {
+			if (postId === "43295") {
 				addCurrentPhotoBookConfig(photobookData);
 				setStatusView("continue");
 				return;
@@ -435,7 +435,7 @@ const CorrectAccessGuard = () => {
 			setStatusView("done");
 			return;
 		}
-		if (postId === "43112") {
+		if (postId === "43295") {
 			addCurrentPhotoBookConfig(photobookData);
 			setStatusView("continue");
 			return;
@@ -461,7 +461,7 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
-				if (postId === "43112") {
+				if (postId === "43295") {
 					addCurrentPhotoBookConfig(photobookData);
 					setStatusView("continue");
 					return;
