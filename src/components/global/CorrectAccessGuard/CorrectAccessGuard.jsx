ca878b855d464535da1721db8c1122af6a550c93
JoabMedel <joab_medel99@hotmail.com>
2025-12-02 18:02:04 -0600

    Chnage access users end book

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -427,7 +427,31 @@ const CorrectAccessGuard = () => {
 	const handlerAvailableExtra = async (idOrderExtra) => {
 		const isPaid = await isPaidExtra(idOrderExtra);
 		if (isPaid) {
+			if (
+				(postId === "42516")||
+				(postId === "37505")||
+				(postId === "43701")||
+				(postId === "43256")||
+				(postId === "43214")||
+				(postId === "41895")
+			) {
+				addCurrentPhotoBookConfig(photobookData);
+				setStatusView("continue");
+				return;
+			}
 			setStatusView("done");
+			return;
+		}
+		if (
+			(postId === "42516")||
+			(postId === "37505")||
+			(postId === "43701")||
+			(postId === "43256")||
+			(postId === "43214")||
+			(postId === "41895")
+		) {
+			addCurrentPhotoBookConfig(photobookData);
+			setStatusView("continue");
 			return;
 		}
 		setStatusView("notPaidExtras");
@@ -451,6 +475,18 @@ const CorrectAccessGuard = () => {
 		}
 		if (photobookData?.meta?.status === "48") {
 			if (!photobookData?.meta?.id_pedido_hojas_extra) {
+				if (
+					(postId === "42516")||
+					(postId === "37505")||
+					(postId === "43701")||
+					(postId === "43256")||
+					(postId === "43214")||
+					(postId === "41895")
+				) {
+					addCurrentPhotoBookConfig(photobookData);
+					setStatusView("continue");
+					return;
+				}
 				setStatusView("done");
 				return;
 			}
