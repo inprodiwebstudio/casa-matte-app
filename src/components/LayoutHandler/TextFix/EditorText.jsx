d863cc2a4520bb1f97c6548c9c0e2dbaa781a06e
JoabMedel <joab_medel99@hotmail.com>
2025-12-08 13:24:54 -0600

    Add new icons toolBar textFix component editor

--- src/components/LayoutHandler/TextFix/EditorText.jsx
+++ src/components/LayoutHandler/TextFix/EditorText.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
-import { CKEditor }  from "@ckeditor/ckeditor5-react";
-import BalloonEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
-import Essentials    from "@ckeditor/ckeditor5-essentials/src/essentials";
-import Bold          from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Paragraph     from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import FontFamily    from "@ckeditor/ckeditor5-font/src/fontfamily";
-import FontColor     from "@ckeditor/ckeditor5-font/src/fontcolor";
-import FontSize      from "@ckeditor/ckeditor5-font/src/fontsize";
-import Alignment     from "@ckeditor/ckeditor5-alignment/src/alignment";
+import { CKEditor }   from "@ckeditor/ckeditor5-react";
+import { Plugin }     from "@ckeditor/ckeditor5-core";
+import BalloonEditor  from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
+import Essentials     from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Bold           from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Paragraph      from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import FontFamily     from "@ckeditor/ckeditor5-font/src/fontfamily";
+import FontColor      from "@ckeditor/ckeditor5-font/src/fontcolor";
+import FontSize       from "@ckeditor/ckeditor5-font/src/fontsize";
+import Alignment      from "@ckeditor/ckeditor5-alignment/src/alignment";
+import FontFamilyIcon from "Resources/svgIcons/fontFamily.svg?raw";
+import FontColorIcon  from "Resources/svgIcons/colorWheel.svg?raw";
 // import "@ckeditor/ckeditor5-build-classic/build/translations/es";
 
 import { useCallback, useRef, useState, useEffect } from "react";
@@ -17,6 +20,31 @@ import { connect, useSelector, shallowEqual }       from "react-redux";
 
 import { bindAll } from "helpers";
 import styles      from "./styles";
+
+class OverrideIcons extends Plugin {
+	init() {
+		const editor = this.editor;
+
+		const factory = editor.ui.componentFactory;
+		const originalFactory = factory.create.bind(factory);
+
+		factory.create = (name, locale) => {
+			const view = originalFactory(name, locale);
+
+			if (name === "fontfamily") {
+				view.buttonView.icon = FontFamilyIcon;
+				view.buttonView.withText = false;
+			}
+
+			if (name === "fontColor") {
+				view.buttonView.icon = FontColorIcon;
+				view.buttonView.withText = false;
+			}
+
+			return view;
+		};
+	}
+}
 
 const EditText = ({
 	isFront,
@@ -89,7 +117,7 @@ const EditText = ({
 	};
 
 	const editorConfiguration = {
-		plugins      : [Essentials, Bold, Alignment, Paragraph, FontFamily, FontSize, FontColor],
+		plugins      : [Essentials, Bold, Alignment, Paragraph, FontFamily, FontSize, FontColor, OverrideIcons],
 		GroupHeading : false,
 		alignment    : {
 			options : [ "left", "right", "center", "justify" ],
