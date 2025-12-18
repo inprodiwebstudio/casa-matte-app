ea2e66d40ff6499418217b935f7c2c18121919d6
JoabMedel <joab_medel99@hotmail.com>
2025-12-17 19:20:50 -0600

    Log is in onePage

--- src/helpers/Hooks/useWorkspaceScale.js
+++ src/helpers/Hooks/useWorkspaceScale.js
@@ -35,19 +35,14 @@ const useWorkspaceScale = () => {
 			onePageBigTablet
 		) => {
 			if (isOnePage) {
+				console.log("isInOnePage");
 				if (width <= 1200) return onePageBigTablet ?? onePageBase;
 				if (width <= 1500) return onePageLaptop ?? onePageBase;
 				if (width <= 1800) return onePageBigLaptop ?? onePageBase;
 				return onePageBase;
 			} else {
-				if (width <= 1200) {
-					console.log("Es bigTablet");
-					return bigTablet ?? base;
-				}
-				if (width <= 1500) {
-					console.log("Es laptop");
-					return laptop ?? base;
-				}
+				if (width <= 1200) return bigTablet ?? base;
+				if (width <= 1500) return laptop ?? base;
 				if (width <= 1800) return bigLaptop ?? base;
 				return base;
 			}
