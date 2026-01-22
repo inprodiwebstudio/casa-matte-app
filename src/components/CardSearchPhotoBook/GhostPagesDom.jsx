c2dd4d6b2a8735db423e6fd80210a05d3d743e6a
JoabMedel <joab_medel99@hotmail.com>
2026-01-22 17:05:42 -0600

    Test view pages spread

--- src/components/CardSearchPhotoBook/GhostPagesDom.jsx
+++ src/components/CardSearchPhotoBook/GhostPagesDom.jsx
@@ -27,7 +27,7 @@ const GhostPagesDom = ({spreadPage}) => {
 			}}>
 			<div
 				style={{
-					height   : "0px",
+					height   : "800px",
 					width    : "100%",
 					overflow : "hidden",
 				}}
