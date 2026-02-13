fcdfa5311e2d9487b5da940d676a2094efa11f58
JoabMedel <joab_medel99@hotmail.com>
2026-02-13 11:35:41 -0600

    Create logs dev download

--- src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
+++ src/components/CardSearchPhotoBook/PhotoBookDownload.jsx
@@ -61,6 +61,7 @@ const PhotoBookDownload = ({ photoBookData, onReturn }) => {
 			return match.replace(/font-size:20px/g, "font-size:8px");
 		});
 		const parseJSON = JSON.parse(cleanedData);
+		console.log(parseJSON);
 		const pagesList = convertToArray(parseJSON?.pages);
 		try {
 			const postTypeId = parseJSON?.postTypeId;
