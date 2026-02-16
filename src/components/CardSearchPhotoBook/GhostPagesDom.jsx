edea0fe4d4528501d4b2706fb5f8c0e29c9b9a34
JoabMedel <joab_medel99@hotmail.com>
2026-02-15 22:24:05 -0600

    delete logs and disabled transforms

--- src/components/CardSearchPhotoBook/GhostPagesDom.jsx
+++ src/components/CardSearchPhotoBook/GhostPagesDom.jsx
@@ -2,10 +2,8 @@ import { shallowEqual, useSelector } from "react-redux";
 import "./GhostTextPageDom.scss";
 import SpreadBook                    from "components/global/SpreadBook";
 import ModLayoutHandler              from "components/global/ModLayoutHandler";
-import { useLocation }               from "react-router";
 
 const GhostPagesDom = ({spreadPage}) => {
-	const location = useLocation();
 	const productPhotoBook = useSelector((state) => state.workSpaceSlice.data?.product, shallowEqual);
 	const workSpaceFormatPage = useSelector((state) => state.workSpaceSlice.data?.format, shallowEqual);
 	const workSpaceSizePage = useSelector((state) => state.workSpaceSlice.data?.sizePhotoBook, shallowEqual);
@@ -20,8 +18,6 @@ const GhostPagesDom = ({spreadPage}) => {
 		}
 		return `${workSpaceFormatPage}-${workSpaceSizePage}`;
 	};
-
-	console.log(location.pathname);
 
 	return (
 		<div
