f4738aad17ea2adc1a1c9cd02904f0bf1a9253f7
JoabMedel <joab_medel99@hotmail.com>
2026-03-04 15:52:17 -0600

    Change page consume get filtered data

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -377,7 +377,7 @@ const Header = () => {
 											radius={12}
 											size="xs"
 											color="darkCasaMatte"
-											onClick={() => getPostIdsEnd(20)}
+											onClick={() => getPostIdsEnd(1)}
 											disabled={false}
 										>
 											PostIdsEnd
