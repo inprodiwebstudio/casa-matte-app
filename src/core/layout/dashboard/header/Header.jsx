92177ee1b7a7c527846b4a86a0f3059311614dc4
JoabMedel <joab_medel99@hotmail.com>
2026-03-09 16:36:31 -0600

    Change page

--- src/core/layout/dashboard/header/Header.jsx
+++ src/core/layout/dashboard/header/Header.jsx
@@ -377,7 +377,7 @@ const Header = () => {
 											radius={12}
 											size="xs"
 											color="darkCasaMatte"
-											onClick={() => getPostIdsEnd(11)}
+											onClick={() => getPostIdsEnd(12)}
 											disabled={false}
 										>
 											PostIdsEnd
