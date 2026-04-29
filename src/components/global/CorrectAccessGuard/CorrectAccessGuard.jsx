f44f80922854b2e56290b12da799e9e84042bafc
JoabMedel <joab_medel99@hotmail.com>
2026-04-29 16:13:25 -0600

    access new user endbook

--- src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
+++ src/components/global/CorrectAccessGuard/CorrectAccessGuard.jsx
@@ -449,7 +449,8 @@ const CorrectAccessGuard = () => {
 				(postId === "46568")||
 				(postId === "41730")||
 				(postId === "51535")||
-				(postId === "95701")
+				(postId === "95701")||
+				(postId === "95596")
 			) {
 				addCurrentPhotoBookConfig(photobookData);
 				setStatusView("continue");
@@ -492,7 +493,8 @@ const CorrectAccessGuard = () => {
 			(postId === "46568")||
 			(postId === "41730")||
 			(postId === "51535")||
-			(postId === "95701")
+			(postId === "95701")||
+			(postId === "95596")
 		) {
 			addCurrentPhotoBookConfig(photobookData);
 			setStatusView("continue");
@@ -553,7 +555,8 @@ const CorrectAccessGuard = () => {
 					(postId === "46568")||
 					(postId === "41730")||
 					(postId === "51535")||
-					(postId === "95701")
+					(postId === "95701")||
+					(postId === "95596")
 				) {
 					addCurrentPhotoBookConfig(photobookData);
 					setStatusView("continue");
